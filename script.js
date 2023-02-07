b7b75930e9c3c4214058425bad411280ad34d0dd
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-07 20:20:37 +0300

    js_20

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
 "use strict";
-//exr1
-let num = 4;
-console.log(num);
 
-//exr2
-let num1 = 3;
-let num2 = 1;
-let num3 = 113;
-
-console.log(num1);
-console.log(num2);
-console.log(num3);
-
-//exr3
-console.log(fdf);
-console.log('fdf');
-
-//exr4
-alert(a);
+const PI = 3.14;
+let a = 6;
+result = 2 * PI *removeEventListener;
+alert(result);
