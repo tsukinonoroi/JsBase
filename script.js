f426e118bd9181517756570672bcaeea3ec0730e
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-07 20:24:26 +0300

    js_27

--- script.js
+++ script.js
@@ -1,65 +1,18 @@
 "use strict";
-let a, b, c, d, result, str;
 //exr1
-let test = Boolean(3);
-alert(test);
+let str = 'abcde';
+
+alert(str[0]); // выведет 'a'
+alert(str[2]); // выведет 'c'
+alert(str[4]); // выведет 'e'
+
 //exr2
-test = Boolean(0);
-alert(test);
+alert(str[4]); // выведет 'e'
+alert(str[3]); // выведет 'd'
+alert(str[2]); // выведет 'c'
+alert(str[1]); // выведет 'b'
+alert(str[0]); // выведет 'a'
+
 //exr3
-test = Boolean(-1);
-alert(test);
-//exr4
-test = Boolean(-0);
-alert(test);
-// exr5
-test = Boolean(+0);
-alert(test);
-//exr6
-test = Boolean('abc');
-alert(test);
-//exr7
-test = Boolean('');
-alert(test);
-//exr8
-test = Boolean('0');
-alert(test);
-//exr9
-test = Boolean(true);
-alert(test);
-//exr10
-test = Boolean(false);
-alert(test);
-//exr11
-test = Boolean('true');
-alert(test);
-//exr12
-test = Boolean('false');
-alert(test);
-//exr13
-test = Boolean(null);
-alert(test);
-//exr14
-test = Boolean('null');
-alert(test);
-//exr15
-test = Boolean(undefined);
-alert(test);
-//exr16
-test = Boolean('undefined');
-alert(test);
-//exr17
-test = Boolean(NaN);
-alert(test);
-//exr18
-test = Boolean('NaN');
-alert(test);
-//exr19
-test = Boolean(3 * 'abc');
-alert(test);
-//exr20
-test = Boolean(Infinity);
-alert(test);
-//exr21
-test = Boolean(1 / 0);
-alert(test);
+let num = 3;
+alert(str[num]);
