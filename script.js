3136940aa4a96d55779a9c41d1387aedcc0ff87a
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-07 20:14:41 +0300

    js_15

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 "use strict";
+// exr1
+let str;
+str = '!!!'
+alert(str);
 
-let str = 'Edemchik';
-alert(str); 
+// exr2
+let a;
+let b;
+a = 'java';
+b = 'script';
+alert(a + b);
+
+// exr3
+let c;
+let d;
+c = 'hello';
+d = 'world';
+alert(c + d);
+
