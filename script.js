56f6ad1d4094d4467b8444b94573ae491a1b5531
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-07 20:23:46 +0300

    js_26

--- script.js
+++ script.js
@@ -1,23 +1,65 @@
 "use strict";
 let a, b, c, d, result, str;
-
 //exr1
-alert(true + 3);
-
+let test = Boolean(3);
+alert(test);
 //exr2
-alert(true + true);
-
+test = Boolean(0);
+alert(test);
 //exr3
-alert(true - true);
-
+test = Boolean(-1);
+alert(test);
 //exr4
-alert(true + false);
-
-//exr5
-alert('1' + true);
-
+test = Boolean(-0);
+alert(test);
+// exr5
+test = Boolean(+0);
+alert(test);
 //exr6
-alert( String(true) + 1 );
-
+test = Boolean('abc');
+alert(test);
 //exr7
-alert( String(true) + Number(true) );
+test = Boolean('');
+alert(test);
+//exr8
+test = Boolean('0');
+alert(test);
+//exr9
+test = Boolean(true);
+alert(test);
+//exr10
+test = Boolean(false);
+alert(test);
+//exr11
+test = Boolean('true');
+alert(test);
+//exr12
+test = Boolean('false');
+alert(test);
+//exr13
+test = Boolean(null);
+alert(test);
+//exr14
+test = Boolean('null');
+alert(test);
+//exr15
+test = Boolean(undefined);
+alert(test);
+//exr16
+test = Boolean('undefined');
+alert(test);
+//exr17
+test = Boolean(NaN);
+alert(test);
+//exr18
+test = Boolean('NaN');
+alert(test);
+//exr19
+test = Boolean(3 * 'abc');
+alert(test);
+//exr20
+test = Boolean(Infinity);
+alert(test);
+//exr21
+test = Boolean(1 / 0);
+alert(test);
