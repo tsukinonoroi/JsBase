7300dce1c756e57d521ca07658a047f9b78abfe4
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-07 20:18:24 +0300

    js_17

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 "use strict";
 
-let str = 'LDLLSAKSDaldsladsalf:sadfldsfvadfCFKFKKKFLDFDSLFDASDC;WAD;F;F;';
-alert(str.length); //Выведет колво символов строки выше
+let str = `abc
+def`; 
+alert(str);
+
+let a, b, c,d; 
+
+
+a = 'java'.length;
+b = 'script';
+c = 'hello';
+d = ' world';
+alert(a);
+alert(c+d);
+
+let str1 = 'xxx';
+let str2 = 'yyy';
+let txt  = `aaa${str1}bbb${str2}ccc`;
+alert(txt);
+
+let abc = `l
+h
+g
+`;
