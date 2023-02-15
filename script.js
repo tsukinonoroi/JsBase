329ece7da288bf63d7d7ccc6c386b0c71b7a3de6
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-15 19:19:24 +0300

    js_38

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 "use strict";
 
-let r_circle = Number(prompt("Enter radius of circle: ")),
-  Pi = Math.PI;
-document.write(`Задание 1: ${Pi * r_circle ** 2}`);
-let a_square = Number(prompt("Enter length of square: "));
-document.write(`<br>Задание 2: ${a_square ** 2}`);
-let l_rect = Number(prompt("Enter length of rectangle: ")),
-  w_rect = Number(prompt("Enter width of rectangle: "));
-document.write(`<br>Задание 3: ${w_rect * l_rect}`);
-document.write(`<br>Задание 4: ${2 * (l_rect + w_rect)}`);
-let tc = Number(prompt("Enter temperature in Celcius: "));
-document.write(`<br>Задание 5: ${tc * (9 / 5) + 32}`);
-let fc = Number(prompt("Enter temperature in Fahrenheit: "));
-document.write(`<br>Задание 6: ${(fc - 32) / (9 / 5)}`);
+let arr_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+  arr_str = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"];
+alert(arr_num);
+console.log(arr_num);
+alert(arr_str);
+console.log(arr_str);
