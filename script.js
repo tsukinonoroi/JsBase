2d96d75652fd5f1fe9542a183a854d31df822335
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-15 19:30:26 +0300

    js_49

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 "use strict";
 
 let obj = {
-  1: "Monday",
-  2: "Tuesday",
-  3: "Wednesday",
-  4: "Thursday",
-  5: "Friday",
-  6: "Saturday",
-  7: "Sunday",
+  1: "January",
+  2: "February",
+  3: "March",
+  4: "April",
+  5: "May",
+  6: "June",
+  7: "July",
+  8: "August",
+  9: "September",
+  10: "October",
+  11: "November",
+  12: "December",
 };
-for (let index = 0; index < 7; index++) {
-  document.write(`${obj[index + 1]}<br>`);
+for (let index = 0; index < 12; index++) {
+  console.log(obj[index + 1]);
 }
