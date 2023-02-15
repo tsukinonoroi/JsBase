f818357e6026964aa54db74b6966d0d419242b7b
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-15 19:32:13 +0300

    js_50

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 "use strict";
 
-let obj = {
-  1: "January",
-  2: "February",
-  3: "March",
-  4: "April",
-  5: "May",
-  6: "June",
-  7: "July",
-  8: "August",
-  9: "September",
-  10: "October",
-  11: "November",
-  12: "December",
-};
-for (let index = 0; index < 12; index++) {
-  console.log(obj[index + 1]);
-}
+let user = { name: "Edem", surname: "Seytumerov", patronymic: "Tsuki No Noroi" };
+document.write(`${user.name} ${user.surname} ${user.patronymic}`);
