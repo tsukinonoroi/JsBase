f8171b4471155d890e97ba587e0aa5472ce6dd4e
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-15 19:14:23 +0300

    js_36

--- script.js
+++ script.js
@@ -1,55 +1,12 @@
 "use strict";
 
-// Задание 1
-let num1 = 1;
-let num2 = 2;
-console.log(`сумма: ${num1 + num2}`);
-// Задание 2
-let a = 1;
-let b = 2;
-console.log(`${a + b}`);
-// Задание 3
-let num = '123';
-let sum = Number(num[0]) + Number(num[1]) + Number(num[2]);
-console.log(sum);
-// Задание 4
-let num_new = 123;
-num_new = String(num_new);
-console.log(num_new[0]);
-// Задание 5
-let c = 0;
-console.log(++c);
-// Задание 6
-let num_new_new = 123;
-num_new_new = String(num_new_new);
-console.log(num_new_new.length);
-// Задание 7
-let d = 24 * 60 * 60;
-console.log(d);
-// Задание 8
-let num_bebra = 123;
-let str = String(num_bebra);
-console.log(str.length);
-// Задание 9
-let number = 123;
-let string = String(number);
-console.log(Number(string[string.length - 1]));
-// Задание 10
-let num_HVATIT = 123;
-let str_HVATIT = String(num_HVATIT);
-console.log(str_HVATIT.length);
-// Задание 11
-let num_AAAAAGH = 123;
-let str_AAAAAGH = String(num_AAAAAGH);
-console.log(str_AAAAAGH[str_AAAAAGH.length - 1]);
-// Задание 12
-let e = '123';
-let g = '456';
-let s = Number(e) + Number(g);
-console.log(s);
-// Задание 13
-let aaa = 1;
-let bbb = 2;
-let ccc = 3;
-
-console.log(aaa + bbb + ccc);
+document.write(`Задание 1: ${24 * 60 * 60}<br>`);
+document.write(`Задание 2: ${30 * 24 * 60 * 60}<br>`);
+document.write(`Задание 3: ${365 * 24 * 60 * 60}<br>`);
+document.write(`Задание 4: ${24 * 60}<br>`);
+document.write(`Задание 5: ${365 * 24 * 60}<br>`);
+document.write(`Задание 6: ${1024 * 1024}<br>`);
+document.write(`Задание 7: ${1024 * 1024 * 1024}<br>`);
+document.write(`Задание 8: ${1024 * 1024 * 1024 * 10}<br>`);
+document.write(`Задание 9: ${1024 * 1024 * 1000}<br>`);
+document.write(`Задание 10: ${1024 * 1024 * 1024 * 1000}<br>`);
