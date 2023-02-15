0ef72481649491e02a67048fefcc7a5dfb73a6d5
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-15 19:37:31 +0300

    js_56

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 "use strict";
 
-let obj = { 7: "a", 50: "b", 23: "c" };
-
-console.log(obj[50]);
-console.log(obj[23]);
-console.log(obj[7]);
+let obj = { x: 1, y: 2, z: 3 }; 
+let keys = Object.keys(obj);
+console.log(keys);
