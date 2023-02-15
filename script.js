9953f2cec0c5461273b41b09163a78141e4fd28a
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-15 19:24:04 +0300

    js_43

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let arr = [];
-(arr[0] = 1), (arr[1] = 2), (arr[2] = 3);
-document.write(`Задание 1: ${arr}<br>`);
-(arr[3] = 4), (arr[4] = 5);
-document.write(`Задание 2: ${arr}`);
+
+arr[3] = "a";
+arr[8] = "b";
+document.write(`Длина: ${arr.length}<br>Массив: ${arr}`);
