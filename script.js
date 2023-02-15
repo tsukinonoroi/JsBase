3f558cef5646161a5df3a41f1cc1508292355606
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-15 19:35:31 +0300

    js_53

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 "use strict";
 
-let obj = {
-  "1a": 1,
-  b2: 2,
-  "c-c": 3,
-  "d 4": 4,
-  e5: 5,
-};
-
-console.log(obj['1a']);
-console.log(obj.b2);
-console.log(obj['c-c']);
-console.log(obj['d 4']);
-console.log(obj.e5);
+let obj = { x: 1, y: 2, z: 3 };
+obj.x **= 2;
+obj.y **= 2;
+obj.z **= 2;
+console.log(obj);
