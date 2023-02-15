95e010432bb4736fb2acbed4eb788dec34d83724
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-15 19:21:38 +0300

    js_39

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 "use strict";
 
-let arr_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-  arr_str = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"];
-alert(arr_num);
-console.log(arr_num);
-alert(arr_str);
-console.log(arr_str);
+let arr_num = [1, 2, 3],
+  arr_str = ["a", "b", "c", "d"];
+document.write(
+  `Задание 1: ${arr_num[0]}<br>${arr_num[1]}<br>${arr_num[2]}<br>`
+);
+document.write(`Задание 2: ${arr_num[0]}, ${arr_num[1]}, ${arr_num[2]}<br>`);
+document.write(`Задание 3: ${arr_num[0] + arr_num[1] + arr_num[2]}<br>`);
+document.write(
+  `Задание 4: ${arr_str[0]}+${arr_str[1]}+${arr_str[2]}+${arr_str[3]}`
+);
