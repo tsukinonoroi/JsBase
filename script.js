a62374467bd1ae64f6ff72dbf2e8f914ff94e18e
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-15 19:08:54 +0300

    js_33

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 "use strict";
 
-alert(0.1 * 0.2);
-alert(0.3 - 0.1);
+alert(`Задание 1: Ваш возраст: ${Number(prompt('Ваш возраст?'))}`);
+let num1 = Number(prompt('Введите первое число'));
+let num2 = Number(prompt('Введите второе число'));
+
+alert(`Задание 2: ${Number(num1) + Number(num2)}`);
+let area = Number(prompt('Введите длину стороны квадрата: '));
+alert(`Задание 3: ${area ** 2}`);
+num1 = Number(prompt('Введите длину прямоугольника'));
+num2 = Number(prompt('Введите ширину прямоугольника'));
+alert(`Задание 4: ${Number(num1) * 2 + Number(num2) * 2}`);
