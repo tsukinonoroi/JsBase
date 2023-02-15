dbe04aa5035610bb4e645debdd8aaa356d07de4a
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-15 19:34:20 +0300

    js_51

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 "use strict";
 
-let user = { name: "Edem", surname: "Seytumerov", patronymic: "Tsuki No Noroi" };
-document.write(`${user.name} ${user.surname} ${user.patronymic}`);
+let today = new Date();
+var d = String(today.getDate()).padStart(2, "0");
+var m = String(today.getMonth() + 1).padStart(2, "0");
+var y = today.getFullYear();
+console.log(y);
+let date = { year: y, month: m, day: d };
+document.write(`${date.year}-${date.month}-${date.day}`);
