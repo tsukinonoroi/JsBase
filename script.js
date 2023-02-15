8711a9e069a35b87b1179f7a03e583cb9b2d5851
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-15 19:26:04 +0300

    js_48

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 "use strict";
 
-let arr = [1, 2, 3, 4, 5];
-console.log(`Задание 1: ${arr[arr.length - 1]}`);
-console.log(`Задание 2: ${arr[0] + arr[1] + arr[2] + arr[3] + arr[4]}`);
-console.log(`Задание 3: ${arr.length}`);
-console.log(`Задание 4: ${arr.length}`);
+let obj = {
+  1: "Monday",
+  2: "Tuesday",
+  3: "Wednesday",
+  4: "Thursday",
+  5: "Friday",
+  6: "Saturday",
+  7: "Sunday",
+};
+for (let index = 0; index < 7; index++) {
+  document.write(`${obj[index + 1]}<br>`);
+}
