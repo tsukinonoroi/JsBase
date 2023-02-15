16bf095977a537a44826d64ce48d9a347b042302
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-15 19:04:56 +0300

    js_30

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-function reverseString(str) {
-    return str.split("").reverse().join("");
-}
-let str = '12345', num = 12345, sum_str = 0, sum_num = 0, comp_num = 1;
-for (let index = 0; index < str.length; index++) {
-    sum_str += Number(str[index]);
-};
-num = String(num);
-for (let index = 0; index < num.length; index++) {
-    sum_num += Number(num[index]);
-};
-for (let index = 0; index < num.length; index++) {
-    comp_num *= Number(num[index]);
-};
+"ust strict";
 
-alert(`Задание 1: ${sum_str}`);
-alert(`Задание 2: ${sum_num}`);
-alert(`Задание 3: ${comp_num}`);
-alert(`Задание 4: ${reverseString(num)}`);
+let num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+alert(num);
