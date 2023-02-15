a648394e79423e647db3f27e8da7b4dcd00deed1
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-15 19:23:17 +0300

    js_41

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 "use strict";
 
-let arr = ["1", 2, "3", 4, 5, "branches"];
-document.write(`Задание 1: ${arr.length}<br>`);
-document.write(`Задание 2: ${arr[arr.length - 1]}`);
+let arr_1 = ["a", "b", "c"],
+  arr_2 = [1, 2, 3],
+  arr_3 = [1, 2, 3];
+(arr_1[0] = 1), (arr_1[1] = 2), (arr_1[2] = 3);
+document.write(`Задание 1: ${arr_1}<br>`);
+for (let index = 0; index < arr_2.length; index++) {
+  arr_2[index] += 3;
+}
+document.write(`Задание 2: ${arr_2}<br>`);
+for (let index = 0; index < arr_3.length; index++) {
+  arr_3[index]++;
+}
+document.write(`Задание 3: ${arr_3}`);
