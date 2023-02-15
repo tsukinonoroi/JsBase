7f28a086c80df3802b185049b55e6520b4263c14
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-15 19:24:26 +0300

    js_44

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 "use strict";
 
 let arr = [];
-
-arr[3] = "a";
-arr[8] = "b";
-document.write(`Длина: ${arr.length}<br>Массив: ${arr}`);
+for (let index = 0; index < 3; index++) {
+  arr.push(index + 1);
+}
+document.write(`Задание 1: ${arr}<br>`);
+for (let index = 3; index < 5; index++) {
+  arr.push(index + 1);
+}
+document.write(`Задание 2: ${arr}`);
