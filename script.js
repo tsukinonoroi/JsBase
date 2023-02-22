e8db48de5b9452959a7c2a5da7a5f877feb6d14d
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-22 17:38:52 +0300

    js_66

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 "use strict";
 
-// console.log( typeof {x: 1, y: 2, z: 3} );4
-// object
-
-// let arr = [1, 2, 3];
-// console.log( typeof arr );
-// object
-
-// let arr = [1, 2, 3];
-// console.log( typeof arr[0] );
-// number
-
-// let arr = ['1', '2', '3'];
-// console.log( typeof arr[0] );
-//string
+// console.log( Array.isArray([1, 2, 3]) );
+// true
+// console.log( Array.isArray({x: 1, y: 2, z: 3}) );
+// false
