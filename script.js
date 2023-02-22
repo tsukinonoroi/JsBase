9576421ab4367ed37d5db8723b3e3a249949ce6d
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-22 17:41:01 +0300

    js_71

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 "use strict";
 
-const arr = [1, 2, 3, 4, 5];
-const res = arr[1] + arr[2];
+let obj = {x: 1, y: 2, z: 3};
+console.log(obj.x);
 
-console.log(res);
+/* let obj1 = {x: 1, y: 2, z: 3};
+let key = 'x';
+console.log(obj1[key]);
+let obj2 = {x: 1, y: 2, z: 3};
+let sum = obj2.x + obj2.y + obj2.x;
+console.log(sum);
+let obj3 = {x: 1, y: 2, z: 3};
+console.log(Object.keys(obj3).length); */
