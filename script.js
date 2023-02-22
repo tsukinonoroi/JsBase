9d5669ddbb2fda4e93d063a483dcdf0fdcac5ded
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-22 17:42:58 +0300

    js_74

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
 "use strict";
 
-let test = 21;
-if (test > 10) {
-	console.log('> 10');
-} else {
-	console.log('не больше 10');}
-let test1 = 3;
-if (test1 < 10) {
-	console.log('< 10');
-} else {
-	console.log('не меньше 10');}
+let test = 10;
 
-let test2 = 10;
-if (test2 >= 0) {
-	console.log('>=10');
+if (test == 10) {
+	console.log('+++');
 } else {
-	console.log('не больше или не равна 10');}
+	console.log('---');
+}
