1fbbbb46a2eea7ed05bffd5bed2ba44dfa9b8df8
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-22 17:40:15 +0300

    js_69

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 "use strict";
 
-let arr1 = [1, 2, 3];
-let arr2 = arr1;
-
-arr1[0] = 'a';
-console.log(arr2);
-// 'a', 2, 3
-
-
-let arr3 = [1, 2, 3];
-let arr4 = arr3;
-arr3[0] = 'a';
-arr4[1] = 'b';
-console.log(arr3);
+/* const arr = ['a', 'b', 'c'];
+arr[1] = '!';
+console.log(arr);
+const arr = ['a', 'b', 'c'];
+arr = [1, 2, 3];
+console.log(arr);
+const arr = ['a', 'b', 'c'];
+arr = ['a', 'b', 'c'];
+console.log(arr); */
