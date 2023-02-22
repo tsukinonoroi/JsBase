6721f1f9a04816d1af21ac62da8974f37a010c5b
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-22 17:47:44 +0300

    js_83

--- script.js
+++ script.js
@@ -1,55 +1,7 @@
 "use strict";
 
-/* let num1 = -10;
-let num2 = -10;
-if (num1 >= 0 || num2 >= 0) {
+if (num == 0 || num > 1 && num < 5 ) {
 	console.log('+++');
 } else {
 	console.log('---');
-}
-let num1 = 0;
-let num2 = 0;
-if (num1 >= 0 || num2 >= 0) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
-let num1 = 0;
-let num2 = 5;
-if (num1 >= 0 || num2 >= 0) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
-let num1 = 5;
-let num2 = 5;
-if (num1 >= 0 || num2 >= 0) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
-let num1 = -5;
-let num2 = 15;
-if (num1 >= 0 || num2 >= 0) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
-let num = 1;
-if (num == 0 || num == 1) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
-let num = 2;
-if (num == 0 || num == 1) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
-let num = 2;
-if (num == 0 || num == 1 || num == 2) {
-	console.log('+++');
-} else {
-	console.log('---');
-} */
+} // сначала выполняется операция &&, а затем операция || и потом уже остальные
