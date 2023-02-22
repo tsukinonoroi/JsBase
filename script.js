0bfa041fde68039115d3ff21559b3069125f4af5
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-22 17:48:10 +0300

    js_84

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 "use strict";
 
-if (num == 0 || num > 1 && num < 5 ) {
+/* let num = 3;
+		
+if (num > 5 && num < 10 || num == 20) {
 	console.log('+++');
 } else {
 	console.log('---');
-} // сначала выполняется операция &&, а затем операция || и потом уже остальные
+}
+let num = 3;
+		
+if (num > 5 || num > 0 && num < 3) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+let num = 3;
+		
+if (num == 9 || num > 10 && num < 20 || num > 20 && num < 30) {
+	console.log('+++');
+} else {
+	console.log('---');
+} */
