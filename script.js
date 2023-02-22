2c2a9a7e1c4d7af20909c5e49b7191397eb89f3c
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-22 17:39:16 +0300

    js_67

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 "use strict";
 
-// console.log( Array.isArray([1, 2, 3]) );
-// true
-// console.log( Array.isArray({x: 1, y: 2, z: 3}) );
-// false
+/* let test = {x: 1, y: 2, z: 3};
+console.log(test);
+object
+let test = {x: 1, y: 2, z: 3};
+console.log(test.x);
+primitive
+let test = [1, 2, 3];
+console.log(test);
+object
+let test = [1, 2, 3];
+console.log(test[1]);
+primitive
+let test1 = [1, 2, 3];
+let test2 = 1;
+console.log(test1);
+object
+let test1 = [1, 2, 3];
+let test2 = 1;
+console.log(test1[test2]);
+primitive */
