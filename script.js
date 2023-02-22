4d34fbb444df1e5f12c7f0768d4c452c0a4bbf1b
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-22 17:08:15 +0300

    js_57

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 "use strict";
 
-let obj = { x: 1, y: 2, z: 3 }; 
-let keys = Object.keys(obj);
-console.log(keys);
+let obj = { x: 1, y: 2, z: 3 };
+document.write(`Object.keys(obj).length = ${Object.keys(obj).length}`);
