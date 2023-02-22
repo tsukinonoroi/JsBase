fd36256459f6825885660126e0be6fb076d4b461
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-22 17:43:46 +0300

    js_76

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 "use strict";
 
-let test = 10;
-
-if (test != 10) {
-	console.log('+++');
+/* let test1 = 1;
+let test2 = 2;
+if (test2 > test1) {
+console.log('test2 > test1');
 } else {
-	console.log('---');
+	console.log('test2 < test1');
 }
+let test3 = 1;
+let test4 = 2;
+if (test3 == test4) {
+console.log('test3 == test4');
+} else {
+console.log('test3 != test4');
+} */
