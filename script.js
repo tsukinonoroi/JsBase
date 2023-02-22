aac0afeffa8151a2bb0b6846e67c7edefcf12350
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-22 17:41:24 +0300

    js_72

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 "use strict";
 
-let obj = {x: 1, y: 2, z: 3};
-console.log(obj.x);
-
-/* let obj1 = {x: 1, y: 2, z: 3};
-let key = 'x';
-console.log(obj1[key]);
-let obj2 = {x: 1, y: 2, z: 3};
-let sum = obj2.x + obj2.y + obj2.x;
-console.log(sum);
-let obj3 = {x: 1, y: 2, z: 3};
-console.log(Object.keys(obj3).length); */
+if (ko) {
+} else {
+}
