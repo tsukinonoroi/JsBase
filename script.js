b36987fc12360b01399327ecbc4b0d696cc95943
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-22 17:38:12 +0300

    js_65

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 "use strict";
 
-let obj = {x: 1, y: 2, z: 3};
-document.write( typeof obj['x'] );
-//number
+// console.log( typeof {x: 1, y: 2, z: 3} );4
+// object
 
-//все остальные - тип object
+// let arr = [1, 2, 3];
+// console.log( typeof arr );
+// object
+
+// let arr = [1, 2, 3];
+// console.log( typeof arr[0] );
+// number
+
+// let arr = ['1', '2', '3'];
+// console.log( typeof arr[0] );
+//string
