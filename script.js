ab6c4a67ebf7edfdbcae6ebc74ea7086eac70152
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-03-01 20:17:03 +0300

    js_103

--- script.js
+++ script.js
@@ -1,55 +1,19 @@
 "use strict";
 
-/* let age = 17;
-let adult;
-if (age >= 18) {
-	adult = true;
-} else {
-	adult = false;    ошибка исправлена 
-}
-console.log(adult); */
+let min = 10;
 
-
-
-/* let age = 17;
-let adult;
-if (age >= 18) {
-	adult = true;
-} else {
-	adult = false;    ошибка исправлена 
-}
-console.log(adult); */
-
-
-
-/* let age = 17;
-let res;
-if (age >= 18) {
-	if (age <= 23) {
-		res = 'от 18 до 23';
-	} else {
-		res = 'больше 23';
-	}
-} else {
-	res = 'меньше 18';    ошибка исправлена 
-}
-document.write(res); */
-
-
-
-
-let age = 19;
-let res;
-
-if (age >= 18) {
-	res;
-	if (age <= 23) {
-		res = 'от 18 до 23';
-	} else {
-		res = 'больше 23';
-	}
-} else {
-	res = 'меньше 18';
+if (min >= 0 && min <= 14) {
+	document.write('1 четверть');
 }
 
-document.write(res);
+if (min >= 15 && min <= 29) {
+	document.write('2 четверть');
+}
+
+if (min >= 30 && min <= 44) {
+	document.write('3 четверть');
+}
+
+if (min >= 45 && min <= 59) {
+	document.write('4 четверть');
+}
