7d5ad0270e595f2ab846e94a2a176c55e297e2e4
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-03-01 20:15:03 +0300

    js_99

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 "use strict";
 
-let num = 1;
-let res = num >= 0 ? '1':'2';
-console.log(res);
+let a = 2 * (3 - 1);
+let b = 6 - 2;
+document.write(a == b, '<br>')
+
+let c = 5 * (7 - 4);
+let d = 1 + 2 + 7;
+document.write(c > d, '<br>')
+
+let e = 2 ** 4;
+let f = 4 ** 2;
+document.write(e != f)
