fd1316c08db21816366019d1b3aa1401354a94f5
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-03-01 20:09:06 +0300

    js_89

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 "use strict";
 
 let test = true;
-if (test) {
+if (!test) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+// 2
+let test1 = true;
+if (!test1) {
 	console.log('+++');
 } else {
 	console.log('---');
