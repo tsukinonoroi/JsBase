9101178f2375f846d189c12bd6d25fb001bd1c84
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-03-01 20:21:01 +0300

    js_112

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
 "use strict";
 
-for (let i = 1; i <= 100; i++) {
-	console.log(i);
-}
-
-for (let i = 11; i <= 33; i++) {
-	console.log(i);
-}
-
-for (let i = 0; i <= 100; i += 2) {
-	console.log(i);
-}
-
-for (let i = 1; i <= 99; i += 2) {
-	console.log(i);
-}
-
-for (let i = 100; i > 0; i--) {
-	console.log(i);
+let arr = ['e', 'd', 'e', 'm'];
+for (let elem of arr) {
+	console.log(elem);
 }
