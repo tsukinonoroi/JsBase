befb474c70fcf315eed6a4be8ec28ab993d9a12c
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-03-01 20:19:28 +0300

    js_109

--- script.js
+++ script.js
@@ -1,64 +1,54 @@
 "use strict";
 
-let num1 = 1;
-let num2 = 2;
+let month = 5;
+if (month >= 1 && month < 3) {
+document.write('It is winter now.', '<br>');
+} else if (month >= 3 && month < 6) {
+document.write('It is spring now.', '<br>')
+} else if (month >= 6 && month < 9) {
+document.write('It is summer now.', '<br>')
+} else if (month >= 9 && month <= 11) {
+document.write('It is fall now.', '<br>');
+} else if (month === 12) {
+document.write('It is winter now.', '<br>');
+} else 
+document.write('Error!');
 
-if (num1 + num2 === 3) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
 
-let num1 = 1;
-let num2 = 2;
 
-if (num1 + num2 === 3) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
+let str = 'abcde';
+if (str[0] === 'a') {
+document.write('да', '<br>');
+} else 
+document.write('нет', '<br>');
 
-let num1 = '1';
-let num2 = '2';
 
-if (Number(num1) + Number(num2) === 3) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
 
-let num = 123;
+let num = 12345;
+let str1 = String(num);
 
-if (num[0] == 1) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
+if (str1[0] == 1 || str1[0] == 2 || str1[0] == 3) {
+document.write('да', '<br>');
+} else 
+document.write('нет', '<br>');
 
-let num = 123;
 
-if (String(num[0]) == 1) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
 
-let num = 123;
+let num1 = 123;
+let str2 = String(num1);
 
-if (String(num[0]) === 1) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
+document.write(+str2[0] + +str2[1] + +str2[2], '<br>');
 
-let num = '123033'; // берем в кавычки, чтобы обращаться к цифрам
-let str = String(num);
 
-let sum1 = str[0] + str[1] + str[2];
-let sum2 = str[3] + str[4] + str[5];
+
+let num2 = 123033; // берем в кавычки, чтобы обращаться к цифрам
+let str3 = String(num2);
+
+let sum1 = +str3[0] + +str3[1] + +str3[2];
+let sum2 = +str3[3] + +str3[4] + +str3[5];
 
 if (sum1 == sum2) {
-	console.log('суммы равны');
+	document.write('суммы равны');
 } else {
-	console.log('суммы не равны');
+	document.write('суммы не равны');
 }
