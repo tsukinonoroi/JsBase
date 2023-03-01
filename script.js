3806f29551586d6a014d74da660cdbc5cafaa6a5
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-03-01 20:16:41 +0300

    js_102

--- script.js
+++ script.js
@@ -1,12 +1,55 @@
 "use strict";
 
-let age = 17;
+/* let age = 17;
 let adult;
-
 if (age >= 18) {
 	adult = true;
 } else {
-	adult = false;
+	adult = false;    ошибка исправлена 
+}
+console.log(adult); */
+
+
+
+/* let age = 17;
+let adult;
+if (age >= 18) {
+	adult = true;
+} else {
+	adult = false;    ошибка исправлена 
+}
+console.log(adult); */
+
+
+
+/* let age = 17;
+let res;
+if (age >= 18) {
+	if (age <= 23) {
+		res = 'от 18 до 23';
+	} else {
+		res = 'больше 23';
+	}
+} else {
+	res = 'меньше 18';    ошибка исправлена 
+}
+document.write(res); */
+
+
+
+
+let age = 19;
+let res;
+
+if (age >= 18) {
+	res;
+	if (age <= 23) {
+		res = 'от 18 до 23';
+	} else {
+		res = 'больше 23';
+	}
+} else {
+	res = 'меньше 18';
 }
 
-document.write(adult);
+document.write(res);
