f9d73c4741c3bc216dff8c527a73ad5df6980bbe
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-03-01 20:10:30 +0300

    js_92

--- script.js
+++ script.js
@@ -1,35 +1,6 @@
 "use strict";
 
-let test1 = true;
-let test2 = true;
-if (test1 && test2) { 
+let test = 10;
+if (test == 10) {
 	console.log('+++');
-} else {
-	console.log('---');
-}
-// 2
-let test3 = true;
-let test4 = true;
-if (test3 && test4 !== 
-	true) { 
-	console.log('+++');
-} else {
-	console.log('---');
-}
-// 3
-let test5 = true;
-let test6 = true;
-if (!test5 && !test6) { 
-	console.log('+++');
-} else {
-	console.log('---');
-}
-// 7
-let test7 = true;
-let test8 = true;
-let test9 = true;
-if (test7 || test8 && test9) { 
-	console.log('+++');
-} else {
-	console.log('---');
 }
