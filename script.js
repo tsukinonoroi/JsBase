e1964af833bbfa16e86403145cd96eabe302f61b
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-03-01 20:19:05 +0300

    js_108

--- script.js
+++ script.js
@@ -1,17 +1,64 @@
 "use strict";
 
-let a = 10;
+let num1 = 1;
+let num2 = 2;
 
-if (a % 2 == 0) {
-	document.write('четное', '<br>');
+if (num1 + num2 === 3) {
+	console.log('+++');
 } else {
-	document.write('нечетное', '<br>');
+	console.log('---');
 }
 
-let b = 9;
+let num1 = 1;
+let num2 = 2;
 
-if (b % 3 == 0) {
-	document.write('делится');
+if (num1 + num2 === 3) {
+	console.log('+++');
 } else {
-	document.write('не делится');
+	console.log('---');
+}
+
+let num1 = '1';
+let num2 = '2';
+
+if (Number(num1) + Number(num2) === 3) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+let num = 123;
+
+if (num[0] == 1) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+let num = 123;
+
+if (String(num[0]) == 1) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+let num = 123;
+
+if (String(num[0]) === 1) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+let num = '123033'; // берем в кавычки, чтобы обращаться к цифрам
+let str = String(num);
+
+let sum1 = str[0] + str[1] + str[2];
+let sum2 = str[3] + str[4] + str[5];
+
+if (sum1 == sum2) {
+	console.log('суммы равны');
+} else {
+	console.log('суммы не равны');
 }
