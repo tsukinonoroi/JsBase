da5fd80eb01274d43120a54ec3aa07d8f22be80f
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-03-01 20:11:14 +0300

    js_94

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 "use strict";
 
-if (test > 0) 
-	console.log('+++');
-else 
-	console.log('---');
-// 2
-if (test > 0)
-	console.log('+++');
+let day = 12;
+if (day <= 10){
+    console.log("the first decade")
+} else if (day >= 11 && day <= 20){
+    console.log("the second decade")
+} else if (day >= 21 && day<=31){
+    console.log("the thith decade")
+} else {
+    console.log("wrong number")
+}
