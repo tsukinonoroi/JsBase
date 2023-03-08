634859eebc33bb1207a48aa97c18fe719e3a8242
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-03-08 18:50:47 +0300

    js_114

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 "use strict";
 
-let obj = {x: 1, y: 2, z: 3};
-for (let key in obj) {
-	document.write(key, "<br>");
+console.log('Task 1');
+let i = 1;
+
+while (i <= 100) {
+	console.log(i);
+	i++;
 }
 
-let obj1 = {x: 1, y: 2, z: 3};
-for (let key in obj1) {
-	document.write(obj1[key]);
+console.log('Task 2');
+let y = 11;
+
+while (y <= 33) {
+	console.log(y);
+	y++;
 }
+
+console.log('Task 3');
+let num = 23;
+let count = 0;
+
+while (num <= 1000) {
+	num *= 3;
+	count++;
+}
+console.log('Iter count', + count);
+console.log(num);
