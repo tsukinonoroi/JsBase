a96ea677756f7e9d10abcb8a89090910afd93992
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-03-08 18:53:05 +0300

    js_117

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 "use strict";
 
-let arr = [2, 5, 9, 15, 1, 4];
+let res = 0;
 
-console.log('Task 1');
-for (let elem of arr) {
-	if (elem > 3 && elem < 10) {
-		console.log(elem);
+for (let i = 2; i <= 100; i++) {
+	if (i % 2 == 0) {
+		res += i;
 	}
 }
+console.log(res);
 
-let obj = {a: 1, b: 2, c: 3, d: 4, e: 5};
+res = 0;
 
-console.log('Task 2');
-for (let elem in obj) {
-	if (obj[elem] % 2 == 1) {
-		console.log(elem);
+for (let i = 1; i <= 99; i++) {
+	if (i % 2 == 1) {
+		res += i;
 	}
 }
+console.log(res);
+
+res = 1;
+
+for (let i = 1; i <= 20; i++) {
+	res *= i;
+}
+console.log(res);
