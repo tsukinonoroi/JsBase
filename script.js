a46542ed1bf288d5c6505830be5e88fbf50a8906
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-03-08 18:54:57 +0300

    js_121

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
 "use strict";
 
 
+let arr = [1, 2, 0, 4, 5];
 
-for (let i = 10; i <= 1000; i++) {
-	let str = String(i);
-	console.log(str[0]);
-}
-
-
-for (let i = 10; i <= 1000; i++) {
-	let str = String(i);
-	console.log(+str[0] + (+str[1]));
-}
-
-
-for (let i = 10; i <= 1000; i++) {
-	let str = String(i);
-	if (str[0] == '1') {
-		console.log(str);
+for (let elem of arr) {
+	if (elem == 0) {
+		break;
 	}
-	
+	console.log(elem);
 }
 
 
-for (let i = 10; i <= 1000; i++) {
-	let str = String(i);
-	if ((+str[0] + (+str[1])) == 5) {
-		console.log(str);
+arr = [14, 21, 10, 4, -2, 5];
+let res = 0;
+for (let elem of arr) {
+	if (elem < 0) {
+		break;
 	}
+	res += elem;
 }
+console.log(res);
+
+
+arr = [14, 21, 32, 10, 3, -2, 5];
+res = 0;
+for (let elem of arr) {
+	if (elem == 3) {
+		break;
+	}
+	res += 1;
+}
+console.log(res);
+
+
+arr = [14, 21, 32, 10, 3, -2, 5];
+res = 0;
+let count = 0;
+for (let i = 1; res <=100; i++) {
+	res += i;
+	count++;
+}
+console.log(count);
