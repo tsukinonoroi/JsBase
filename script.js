c3a697ef0c39a24dd4476c8c17df0c9f4d3d66e7
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-03-08 18:55:55 +0300

    js_124

--- script.js
+++ script.js
@@ -2,14 +2,7 @@
 
 
 for (let i = 1; i <= 9; i++) {
-	for (let j = 1; j <= 3; j++) {
-		document.write(i);
-	}
+	let num = 3;
 }
 
-
-for (let i = 1; i <= 3; i++) {
-	for (let j = 1; j <= 3; j++) {
-		document.write('\n' + i + j + ' ');
-	}
-}
+console.log(num)
