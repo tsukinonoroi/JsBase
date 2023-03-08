4a3d1997430214a1bfb4f3ca43bc5c01513d69bb
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-03-08 18:55:36 +0300

    js_123

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 "use strict";
 
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-for (let elem of arr) {
-	let result;
-	
-	if (elem % 2 == 0) {
-		result = elem * elem;
-	} else if (elem % 3 == 0) {
-		result = elem * elem * elem;
-	} else {
-		continue;
+for (let i = 1; i <= 9; i++) {
+	for (let j = 1; j <= 3; j++) {
+		document.write(i);
 	}
-	
-	console.log(result);
+}
+
+
+for (let i = 1; i <= 3; i++) {
+	for (let j = 1; j <= 3; j++) {
+		document.write('\n' + i + j + ' ');
+	}
 }
