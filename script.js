28f76cdfd8a25255f9f63e0ef4a51532958047c6
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-03-08 18:58:40 +0300

    js_129

--- script.js
+++ script.js
@@ -1,48 +1,19 @@
 "use strict";
 
 //////
-let arr1 = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-   let arr2 = [1, 2, 3, 4, 5, 6, 7];
-   let newObj1 = {};
-
-   for (let i = 0; i <= 6; i++) {
-      let key = arr2[i];
-      let value = arr1[i]
-
-      newObj1[key] = value
-   };
-
-console.log(newObj1);
-
-//////
-let obj = { a: 1, b: 2, c: 3, d: 4, e: 5 };
-let newObj2 = {};
-
-for (let key in obj) {
-
-   if (obj[key] % 2 == 0) {
-
-	  let newKey = key;
-	  let value = obj[key];
-	  newObj2[newKey] = value;
-
-   } else {
-	  continue
-   };
-};
-
-console.log(newObj2);
-
-//////
-let obj1 = { a: 1, b: 2, c: 3, d: 4, e: 5 };
-let newObj3 = {};
+let obj1 = {x: 1, y: 2, z: 3};
 
 for (let key in obj1) {
-
-   let newKey = obj1[key];
-   let newValue = key;
-
-   newObj3[newKey] = newValue;
+   obj1[key] = obj1[key] ** 2;
 };
-console.log(obj1);
-console.log(newObj3);
+
+console.log(obj1)
+
+//////
+let obj2 = {x: 1, y: 2, z: 3};
+
+for (let key in obj2) {
+   obj2[key] += 1
+};
+
+console.log(obj2)
