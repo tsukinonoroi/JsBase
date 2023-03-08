22c3d8a3a2aaeca730380a8c6df9f78b074c3357
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-03-08 18:54:27 +0300

    js_120

--- script.js
+++ script.js
@@ -1,36 +1,31 @@
 "use strict";
 
-let str = '';
 
-for (let i = 1; i <= 5; i++) {
-	str += '-';
+
+for (let i = 10; i <= 1000; i++) {
+	let str = String(i);
+	console.log(str[0]);
 }
 
-console.log(str);
 
-
-str = '';
-
-for (let i = 1; i <= 9; i++) {
-	str += i;
+for (let i = 10; i <= 1000; i++) {
+	let str = String(i);
+	console.log(+str[0] + (+str[1]));
 }
 
-console.log(str);
 
-
-str = '';
-
-for (let i = 9; i >= 1; i--) {
-	str += i;
+for (let i = 10; i <= 1000; i++) {
+	let str = String(i);
+	if (str[0] == '1') {
+		console.log(str);
+	}
+	
 }
 
-console.log(str);
 
-
-str = '';
-
-for (let i = 1; i <= 9; i++) {
-	str += i + '-';
+for (let i = 10; i <= 1000; i++) {
+	let str = String(i);
+	if ((+str[0] + (+str[1])) == 5) {
+		console.log(str);
+	}
 }
-
-console.log(str);
