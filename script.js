87f1653bd502e478ea7ce32f73d998c58849dd22
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-03-08 18:58:12 +0300

    js_128

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
 "use strict";
 
 //////
-let arr1 = [1, 2, 3, 4, 5];
+let arr1 = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+   let arr2 = [1, 2, 3, 4, 5, 6, 7];
+   let newObj1 = {};
 
-   for (let item in arr1) {
-      arr1[item] = arr1[item] ** 2;
+   for (let i = 0; i <= 6; i++) {
+      let key = arr2[i];
+      let value = arr1[i]
+
+      newObj1[key] = value
    };
-   console.log(arr1)
+
+console.log(newObj1);
 
 //////
-let arr2 = [1, 2, 3, 4, 5];
+let obj = { a: 1, b: 2, c: 3, d: 4, e: 5 };
+let newObj2 = {};
 
-for (let item in arr2) {
-      arr2[item] -= 1
+for (let key in obj) {
+
+   if (obj[key] % 2 == 0) {
+
+	  let newKey = key;
+	  let value = obj[key];
+	  newObj2[newKey] = value;
+
+   } else {
+	  continue
+   };
 };
-console.log(arr2)
+
+console.log(newObj2);
 
 //////
-let arr3 = [1, 2, 3, 4, 5];
+let obj1 = { a: 1, b: 2, c: 3, d: 4, e: 5 };
+let newObj3 = {};
 
-for (let item in arr3) {
-   arr3[item] += 10
+for (let key in obj1) {
+
+   let newKey = obj1[key];
+   let newValue = key;
+
+   newObj3[newKey] = newValue;
 };
-console.log(arr3)
+console.log(obj1);
+console.log(newObj3);
