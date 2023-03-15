4e5c5dbe4b94c5b458e5bbf9a364d820990cfc9f
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-03-15 21:32:56 +0300

    js_134

--- script.js
+++ script.js
@@ -1,38 +1,11 @@
 console.log('====================== 1 ========================')
 
-let arr1 = [1, 2, 3, 4, 5, 6];
+      let arr = [10, 20, 30, 40, 21, 32, 51];
 
-for (let i = 1; i <= arr1.length; i++) {
-    console.log([arr1[i]]);
-};
-
-console.log('====================== 2 ========================')
-
-let res = 0
-
-for (let i = 1; i <= arr1.length; i++) {
-    res = arr1[i] + arr1[i + 1];
-    console.log(res);
-};
-
-
-console.log('====================== 3 ========================')
-
-for (let i = 1; i <= arr1.length; i++) {
-    console.log(arr1[i - 2], arr1[i - 1]);
-};
-
-console.log('====================== 4 ========================')
-
-for (let i = 1; i <= arr1.length; i++) {
-    console.log(arr1[i - 2] + arr1[i - 1]);
-};
-
-console.log('====================== 4 ========================')
-
-let res1 = 0;
-
-for (let i = 1; i <= arr1.length; i++) {
-    res = arr1[i - 1] + arr1[i] + arr1[i + 1];
-    console.log(res1)
-};
+      for (let i of arr) {
+         if (String(i)[0] == '1' || String(i)[0] == '2') {
+            console.log(i);
+         } else {
+            continue;
+         };
+      };
