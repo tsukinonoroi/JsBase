5dc407ee15cb8f423484daeecc331406431bfa47
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-03-15 21:33:38 +0300

    js_135

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 console.log('====================== 1 ========================')
 
-      let arr = [10, 20, 30, 40, 21, 32, 51];
+let obj = { a: 10, b: 20, c: 30, d: 40, e: 50 };
+let sum = 0;
 
-      for (let i of arr) {
-         if (String(i)[0] == '1' || String(i)[0] == '2') {
-            console.log(i);
-         } else {
-            continue;
-         };
-      };
+for (let elem in obj) {
+   if (obj[elem] == 10 || obj[elem] == 20) {
+      sum += obj[elem];
+   } else {
+      continue;
+   };
+};
+
+console.log(sum);
