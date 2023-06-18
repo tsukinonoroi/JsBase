8a61f024e4610f88f9ffa9d8b97572fe2fb6a85f
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:35:48 +0300

    js_263

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 let elem1 = document.querySelector("#elem1");
-let value = elem1.getAttribute("value");
-console.log(value);
+let length = elem1.classList.length;
+console.log(length);
 //  2
 let elem2 = document.querySelector("#elem2");
-let class1 = elem2.getAttribute("class");
-console.log(class1);
+let class1 = elem2.classList;
+for (let classItem of class1) {
+    console.log(classItem)
+}
 //  3
 let elem3 = document.querySelector("#elem3");
-elem3.setAttribute("value", "!!!");
-let value3 = elem3.getAttribute("value");
-console.log(value3);
+elem3.classList.add('xxx');
+let class2 = elem3.classList;
+console.log(class2)
 //  4
 let elem4 = document.querySelector("#elem4");
-elem4.setAttribute("class", "valid");
-let value4 = elem4.getAttribute("class");
-console.log(value4);
-//  5
+elem4.classList.remove('www', 'zzz');
+let class3 = elem4.classList;
+console.log(class3)
+//   5
 let elem5 = document.querySelector("#elem5");
-elem5.removeAttribute('value');
-//  6
+let contains = elem5.classList.contains('ggg');
+    console.log(contains);
+// 6
 let elem6 = document.querySelector("#elem6");
-console.log(elem6.hasAttribute('value'));
+elem6.classList.toggle('zzz');
+let class4 = elem6.classList;
+console.log(class4)
