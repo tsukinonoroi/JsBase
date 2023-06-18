750c4c5379085c7f9f03884446985a4bb54962c2
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:53:24 +0300

    js_317

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let elem = document.querySelector('#elem');
-let start = document.querySelector('#start');
 
-start.addEventListener('click', function() {
-	setInterval(function(self) {
-		console.log(self.value);
-	}, 1000, this);
+elem.addEventListener('click', function() {
+	setTimeout(function() {
+		alert('!');
+	}, 3000);
 });
