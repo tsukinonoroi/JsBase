f08ab1a43eb5f553b758d79ed184db76556bae52
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:43:15 +0300

    js_286

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 window.onload = function() {
-   const mySelect = document.getElementById("mySelect");
-   const options = mySelect.options;
-   
-   for (let i = 0; i < options.length; i++) {
-     options[i].text += " " + options[i].value;
+   const select = document.querySelector('select');
+   for (let i = 0; i < select.options.length; i++) {
+     const option = select.options[i];
+     if (option.selected) {
+       option.textContent += '!';
+     } else {
+       option.textContent += '?';
+     }
    }
+   // 2
+   const mySelect = document.getElementById('mySelect');
+   const myButton = document.getElementById('myButton');
+   
+   myButton.addEventListener('click', () => {
+     mySelect.selectedIndex = mySelect.options.length - 1;
+   });
+   // 3
+   const select1 = document.getElementById('mySelect');
+   const button = document.getElementById('btn');
+   
+   button.addEventListener('click', function() {
+     const selectedOption = select1.options[select1.selectedIndex];
+     alert(selectedOption.textContent);
+   });
+   // 4
+   let select2 = document.getElementById('mySelect');
+   let button1 = document.getElementById('myButton');
+   
+   button1.addEventListener('click', function() {
+     let selectedOption = select2.options[select2.selectedIndex];
+     selectedOption.textContent += '!';
+   });
    }
