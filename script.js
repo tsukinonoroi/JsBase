632378924b377d15767363bdf82250831fb85187
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:32:09 +0300

    js_252

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-let input1 = document.querySelector("#elem1");
-let button1 = document.querySelector("#btn1");
-button1.addEventListener('click', () => {
-    input1.value = 'text'
-})
-// 2
-let button2 = document.querySelector("#btn2");
-let p1 = document.querySelector("#p1");
-button2.addEventListener('click', () => {
-    p1.textContent = input1.value 
-})
+let elem = document.querySelector("#elem");
+elem.addEventListener("focus", function () {
+    console.log(elem.value);
+});
