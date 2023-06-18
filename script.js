5f6212b7839f1d8f1dd381f277c98fc8f4ef4661
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:43:53 +0300

    js_288

--- script.js
+++ script.js
@@ -1,32 +1,7 @@
 window.onload = function() {
-   let textarea = document.querySelector('textarea');
-   let div = document.querySelector('div');
+   let elem = document.querySelector('#elem');
    
-   textarea.addEventListener('input', function() {
-       div.textContent = textarea.value;
+   elem.addEventListener('click', function() {
+   console.log(event);
    });
-   // 2
-   
-     let checkbox = document.querySelector('#checkbox');
-     let button = document.querySelector('#button');
-     let div1 = document.querySelector('#div');
-   
-     button.addEventListener('click', function() {
-       if (checkbox.checked) {
-         div1.textContent = '111';
-       } else {
-         div1.textContent = '222';
-       }
-     });
-   // 3
-   let checkbox1 = document.querySelector('[type="checkbox"]');
-     let button1 = document.querySelector('button');
-     
-     button1.addEventListener('click', function() {
-       if (checkbox1.checked) {
-         console.log('+++');
-       } else {
-         console.log('---');
-       }
-     });
    }
