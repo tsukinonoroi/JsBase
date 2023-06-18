94b801e156523adaf509d4b974a83c9313576216
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:30:23 +0300

    js_246

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-let elem1 = document.querySelector('#elem1');
-let elem2 = document.querySelector('#elem2');
-let elem3 = document.querySelector('#elem3');
-let elem4 = document.querySelector('#elem4');
-let elem5 = document.querySelector('#elem5');
-elem1.addEventListener('click', func);
-elem2.addEventListener('click', func);
-elem3.addEventListener('click', func);
-elem4.addEventListener('click', func);
-elem5.addEventListener('click', func);
+let elem = document.querySelector('#elem');
+elem.addEventListener('click', func1);
+elem.addEventListener('click', func2);
+elem.addEventListener('click', func3);
+function func1() {
+	console.log('1');
+}
 
-function func() {
-	console.log('message');
+function func2() {
+	console.log('2');
+}
+
+function func3() {
+	console.log('3');
 }
