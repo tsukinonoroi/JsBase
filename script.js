4c93a12fef28e3fcd4cfa1cca8e5468c2c25ee78
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:52:07 +0300

    js_313

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-let start = document.querySelector('#start');
-let stop  = document.querySelector('#stop');
-let timerId;
+let elem = document.querySelector('#elem');
 
-start.addEventListener('click', function() {
-	let i = 0;
-	
-	timerId = setInterval(function() {
-		console.log('1')
-	}, 100);
-});
-
-stop.addEventListener('click', function() {
-	clearInterval(timerId);
-});
+setInterval(function() {
+	elem.value = Number(elem.value) + 1;
+}, 1000);
