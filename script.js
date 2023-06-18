3b169ca0971b9388e2f78cd7017132ae0b587cdf
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:28:44 +0300

    js_241

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-let p1 = document.querySelector("#elem1");
-let p2 = document.querySelector("#elem2");
-let p3 = document.querySelector("#elem3");
-console.log(p1);
-console.log(p2);
-console.log(p3);
-// 2
-let pOne = document.querySelector("#block p");
-console.log(pOne);
-// 3
-let p1One = document.querySelector(".block p");
-console.log(p1One);
-// 4
-let www = document.querySelector(".www");
-console.log(www);
+button1 = document.querySelector("#button1");
+button2 = document.querySelector("#button2");
+button3 = document.querySelector("#button3");
+button1.addEventListener("click", function () {
+    document.write(1)
+});
+button2.addEventListener("click", function () {
+    document.write(2)
+});
+button3.addEventListener("click", function () {
+    document.write(3)
+});
