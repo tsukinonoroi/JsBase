743046a631f31fa59e9f06c8bd22b6772901f211
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:34:54 +0300

    js_260

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-let l = document.querySelector(".link");
-let btn = document.querySelector("#btn")
-
-btn.addEventListener("click", func1)
-function func1() {
-    l.textContent += l.href
-    btn.removeEventListener('click', func1);
+let elems = document.querySelectorAll("p");
+for (let elem of elems) {
+    elem.addEventListener("click", func);
 }
-//  2
-let btn2 = document.querySelector("#btn1");
-btn2.addEventListener("click", func2);
-
-function func2() {
-    if (this.textContent < 10) {
-        this.textContent = Number(this.textContent) + 1
-    } else {
-        this.removeEventListener("click", func2);
-    }
+function func() {
+    this.textContent += '!'
+    this.removeEventListener("click", func); 
 }
