a5d3fbb9d1e10b84706e9a48b7e1d6477e1cd56a
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:35:31 +0300

    js_262

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-let list = document.querySelectorAll(".list");
-for (let elem of list) {
-    elem.addEventListener("click", func);
-};
-function func() {
-    if (Number(this.textContent) < 10) {
-        this.textContent = Number(this.textContent) + 1
-    } else {
-        this.removeEventListener('click', func);
-    }
-}
+let elem1 = document.querySelector("#elem1");
+let value = elem1.getAttribute("value");
+console.log(value);
+//  2
+let elem2 = document.querySelector("#elem2");
+let class1 = elem2.getAttribute("class");
+console.log(class1);
+//  3
+let elem3 = document.querySelector("#elem3");
+elem3.setAttribute("value", "!!!");
+let value3 = elem3.getAttribute("value");
+console.log(value3);
+//  4
+let elem4 = document.querySelector("#elem4");
+elem4.setAttribute("class", "valid");
+let value4 = elem4.getAttribute("class");
+console.log(value4);
+//  5
+let elem5 = document.querySelector("#elem5");
+elem5.removeAttribute('value');
+//  6
+let elem6 = document.querySelector("#elem6");
+console.log(elem6.hasAttribute('value'));
