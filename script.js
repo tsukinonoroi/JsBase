c0a322e9593e95f5be8aed504ec670842f7ac8cf
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:29:45 +0300

    js_244

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-button1 = document.querySelector("#button1");
-button2 = document.querySelector("#button2");
-button3 = document.querySelector("#button3");
-button1.addEventListener("click", function () {
-    document.write(1)
-});
-button2.addEventListener("click", function () {
-    document.write(2)
-});
-button3.addEventListener("click", function () {
-    document.write(3)
-});
+let button1 = document.querySelector('#button1');
+button1.addEventListener('click', func1);
+
+function func1() {
+	console.log(1);
+}
+let button2 = document.querySelector('#button2');
+button2.addEventListener('click', func2);
+
+function func2() {
+	console.log(2);
+}
