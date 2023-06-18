00f2cbef9856a5d765a0e83d5171e964a9b3d11c
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:42:37 +0300

    js_284

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 window.onload = function() {
-   const today = new Date();
-   const currentMonth = today.getMonth() + 1;
-   const monthsSelect = document.getElementById("months");
-   const currentMonthOption = monthsSelect.querySelector(`option[value="${currentMonth}"]`);
-   currentMonthOption.selected = true;
+   const input = document.getElementById('input');
+   const select = document.getElementById('select');
+   
+   input.addEventListener('blur', () => {
+     const value = parseInt(input.value);
+     if (!isNaN(value)) {
+       select.selectedIndex = value - 1;
+     }
+   });
+   // 2
+   const select1 = document.getElementById('weekday-select');
+     const today = new Date().getDay();
+     const option = select1.querySelector(`option[value="${today}"]`);
+     option.selected = true;
    }
