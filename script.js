40432f3930412f6228a474edad319a2aa87fd9ab
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:10:20 +0300

    js_203

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-let arr = [2, 4, 6, 8];
-
-let result = arr.some(elem => elem > 0); 
-console.log(result);
+let arr = [1, 2, 3, 4, 5];
+function func(num1, num2, num3, 
+	num4, num5) { 
+return num1 + num2 + num3 + num4 
+		+ num5; 
+}
+console.log(func(...arr));
 // 2
-let arr2 = [2, 4, 6, 8, 30];
-let result2 = arr2.some((elem, index) => elem * index < 30);
-console.log(result2);
+function func2(n1, n2, n3, n4, n5, n6, n7, n8) {
+return (n1 + n2 + n3 + n4) * (n5 + n6 + n7 + n8);
+}
+console.log(func2(1, ...[2, 3, 4], 5, ...[6], ...[7, 8]));
+// 3
+let arr3 = [1, 2, 3, 4, 5];
+let min = Math.min(...arr3);
+console.log(min)
