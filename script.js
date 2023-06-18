7a4531e5e9e7e727cf081ea1c755f0f2e5f772ff
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:45:55 +0300

    js_294

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 let elem = document.querySelector('#elem');
 
 elem.addEventListener('click', function(event) {
-	if (event.ctrlKey) {
-		console.log('нажат Ctrl' + 1);
-	}
-	
-	if (event.altKey) {
-		console.log('нажат Alt'+1);
-	}
-	
-	if (event.shiftKey) {
-		console.log('нажат Shift'+1);
-	}
+	event.preventDefault('/');
+	this.elem = this.elem + ' (' + this.href + ')';
+	console.log('Вы не можете перейти по этой ссылке!');
 });
