a36cb91d39ada156ab92a1d46d7f91d52b107024
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:24:49 +0300

    js_230

--- script.js
+++ script.js
@@ -2,12 +2,9 @@ let now  = new Date();
 let date = new Date(1988, 1, 4, 23, 
 	59, 59); 
 let date2 = new Date(2000, 1, 10, 23, 59, 59)
-
-let diff = date2.getTime() - date.getTime();
-console.log(diff / (1000 * 3600 * 24));
+let diff = date2 - date; 
+console.log(diff);   
 // 2
-let now1  = new Date();
-let date1 = new Date(2003, 12, 31, 23, 
-	59, 59); 
-let diff1 = now.getTime() - date1.getTime();
-console.log(diff1 / (1000 * 60 * 60 * 24 * 31 ))
+console.log(diff / (1000 * 3600 * 24));
+// 3
+console.log(diff / (1000 * 60 * 60 * 24 * 31 ))
