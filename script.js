063f75f85e587ec2e0dde97321982e0881d1bbfd
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:31:53 +0300

    js_251

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let input1 = document.querySelector("#elem1");
 let button1 = document.querySelector("#btn1");
 button1.addEventListener('click', () => {
-    console.log(input1.type)
+    input1.value = 'text'
 })
-//  2
+// 2
 let button2 = document.querySelector("#btn2");
+let p1 = document.querySelector("#p1");
 button2.addEventListener('click', () => {
-    input1.type = "submit"
-    console.log(input1.type)
+    p1.textContent = input1.value 
 })
