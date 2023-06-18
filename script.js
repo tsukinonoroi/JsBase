82f29515f33b2cf5c3fda95ccfd71aeafcf79988
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:26:13 +0300

    js_234

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-function isLeap(year){
-   year = new Date(2020, 2, 0);
-   if (year.getDate() == 29) {
-  return true
-} else {
-  return false
+function checkDate(year, month, day) {
+   if (
+       new Date(year, month, day).getFullYear() == year &&
+       new Date(year, month, day).getMonth() == month &&
+       new Date(year, month, day).getDate() == day
+   ) {
+       return true;
+   } else {
+       return false;
+   }
 }
-}
+console.log(checkDate(2025, 0, 31));
+console.log(checkDate(2025, 0, 32));
