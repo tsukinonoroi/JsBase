658a17f2620715cca327f4c104fa075633cbed19
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:57:54 +0300

    js_332

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-let arr = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]];
-let table = document.querySelector('#table');
+let users = [
+	{
+		name: 'name1',
+		surname: 'surname1',
+		patronymic: 'patronymic1'
+	},
+	{
+		name: 'name2',
+		surname: 'surname2',
+		patronymic: 'patronymic2'
+	},
+	{
+		name: 'name3',
+		surname: 'surname3',
+		patronymic: 'patronymic3'
+	},
+];
 
-for (let subArr of arr) {
+let table = document.getElementById('table');
+
+for (let user of users) {
 	let tr = document.createElement('tr');
 	
-	for (let elem of subArr) {
-		let td = document.createElement('td');
-		td.textContent = elem;
-		tr.appendChild(td);
-	}
+	let td1 = document.createElement('td');
+	td1.textContent = user.name;
+	tr.appendChild(td1);
+	
+	let td2 = document.createElement('td');
+	td2.textContent = user.surname;
+	tr.appendChild(td2);
+	
+	let td3 = document.createElement('td');
+	td3.textContent = user.patronymic;
+	tr.appendChild(td3);
 	
 	table.appendChild(tr);
 }
