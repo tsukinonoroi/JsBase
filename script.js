8f79fb18dd6c759c988acfc7abce684e5fe6675b
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 01:58:05 +0300

    js_168

--- script.js
+++ script.js
@@ -1,65 +1,42 @@
-let arr1 = [2, 4, 6];
+let x = 2, y = 2
 
-function func1(arr) {
-
-   for (let elem of arr) {
-      if (elem % 2 != 0) {
-         return false;
-      }
-   };
-
-   return true;
+function func(a, b) {
+   return a == b;
 };
 
-if (func1(arr1)) {
-   console.log('Все числа четные');
-} else {
-   console.log('Есть нечетные числа')
+console.log(func(x, y));
+
+
+
+
+x = 3;
+y = 2;
+
+function func2(a, b) {
+   return a != b;
 }
 
+console.log(func2(x, y));
 
 
 
 
-let num = 4663;
+x = 3;
+y = 3;
 
-function func2(number) {
-
-   let arrNum = String(number).split('')
-
-   for (let elem of arrNum) {
-      if (elem % 2 != 0) {
-         return false;
-      }
-   };
-
-   return true;
-};
-
-if (func2(num)) {
-   console.log('Все цифры четные');
-} else {
-   console.log('Есть нечетные цифры')
+function func3(a, b) {
+   return a + b >= 10;
 }
 
+console.log(func3(x, y));
 
 
 
-let arr3 = [2, 3, 4, 4, 7];
 
-function func3(arr) {
-   for (let i = 0; i <= arr.length - 1; i++) {
-      if (arr[i] == arr[i+1]) {
-         return true;
-      } else {
-         continue;
-      }
-   };
-   return false;
+let number = -2;
+
+function func4(num) {
+   return num >= 0
 }
 
-if (func3(arr3)) {
-   console.log('Есть два одинаковых элемента подряд');
-} else {
-   console.log('Нет двух одинаковых элемента подряд')
-}
+console.log(func4(number));
