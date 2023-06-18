50e7e129a8f277652e6894f3af863775f8063955
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:56:27 +0300

    js_327

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let parent = document.querySelector('#parent');
-let elem = parent.querySelector('.elem');
 
-let clone = elem.cloneNode(true);
-parent.appendChild(clone);
+let arr = [1, 2, 3, 4, 5];
+
+for (let elem of arr) {
+	let p = document.createElement('p');
+	p.textContent = elem;
+	
+	parent.appendChild(p);
+}
