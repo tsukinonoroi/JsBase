e17ff74d687b631ba0f96c7691dbc1522ee6a5c7
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:59:56 +0300

    js_339

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 let elem = document.querySelector('#elem');
-let input = document.querySelector('#input');
 
-input.value = elem.textContent;
-
-input.addEventListener('blur', function() {
-	elem.textContent = this.value;
+elem.addEventListener('click', function func() {
+	let input = document.createElement('input');
+	input.value = elem.textContent;
+	
+	elem.textContent = '';
+	elem.appendChild(input);
+	
+	input.addEventListener('blur', function() {
+		elem.textContent = this.value;
+		elem.addEventListener('click', func); 
+	});
+	
+	elem.removeEventListener('click', func);
 });
