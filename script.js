127d7473bde27896e438ab5925cbca2ce5caf238
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:04:04 +0300

    js_184

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
-let arr = [
-   function () { return 1 },
-   function () { return 2 },
-   function () { return 3 },
-];
+let newobj = {
+   func1: function () { return 1 },
+   func2: function () { return 2 },
+   func3: function () { return 3 },
+};
+
+let sum = newobj.func1() + newobj.func2() + newobj.func3();
+console.log(sum);
 
 
 
 
 
-console.log(arr[2]());
-
-
-
-
-
-let sum = arr[0]() + arr[1]() + arr[2]();
-
-console.log(sum)
-
-
-
-
-
-for (let func of arr) {
-   console.log(func());
+for (let elem in newobj) {
+   console.log(newobj[elem]())
 };
