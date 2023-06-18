5c03a8dd6369526a4842a241cf6bb885fd64a65a
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:04:25 +0300

    js_185

--- script.js
+++ script.js
@@ -1,16 +1,78 @@
-let newobj = {
-   func1: function () { return 1 },
-   func2: function () { return 2 },
-   func3: function () { return 3 },
+function func1() {
+   return 1;
+}
+
+function func2() {
+   return 2;
+}
+
+function func3() {
+   return 3;
+}
+
+function test(num1, num2, num3) {
+   return num1 + num2 + num3;
+}
+
+console.log(test(func1(), func2(), func3()));
+
+
+
+
+
+let func4 = () => {
+   return 1;
 };
 
-let sum = newobj.func1() + newobj.func2() + newobj.func3();
-console.log(sum);
-
-
-
-
-
-for (let elem in newobj) {
-   console.log(newobj[elem]())
+let func5 = () => {
+   return 2;
 };
+
+let func6 = () => {
+   return 3;
+};
+
+let test1 = (num1, num2, num3) => {
+   return num1 + num2 + num3;
+};
+
+console.log(test1(func4(), func5(), func6()));
+
+
+
+
+
+
+test(function (num) {
+   return num ** 3;
+});
+
+function func() {
+   console.log(func(3));
+}
+
+
+
+
+
+let func = function () {
+   console.log(func(3));
+};
+
+
+
+
+
+
+function test(num, func1, func2) {
+   return func1(num) + func2(num);
+}
+test(
+   2,
+   function (num) {
+      return num * num;
+   },
+   function (num) {
+      return num * num * num;
+   }
+);
