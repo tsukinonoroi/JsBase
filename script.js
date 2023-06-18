6c91e5c55d4cc812e7c10f4bb6cec2902bbd3c41
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:12:35 +0300

    js_210

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-function func() {
-	return ['John', 'Smit', 'development', 
-		'programmer', 2000]; 
-}
-let [name, surname, department, position, salary] = func();
-console.log(surname);
-console.log(name);
+let arr = ['John', 'Smit', 'development', 
+	'programmer', 2000]; 
+let [, , department, position, ] = arr;
+console.log(department)
+console.log(position)
