cdc69156f3a9db5979c074ea6b5652919fd4e2fc
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:58:43 +0300

    js_335

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-let tds = document.querySelectorAll('#table td');
+let parent = document.querySelector('#parent');
 
-for (let td of tds) {
-	td.textContent = '!';
+for (let i = 1; i <= 9; i++) {
+	let p = document.createElement('p');
+	p.textContent = i;
+	
+	p.addEventListener('click', function() {
+		this.remove();
+	});
+	
+	parent.appendChild(p);
 }
