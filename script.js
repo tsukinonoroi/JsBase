39abf73b69d4bb17b899b56dce26eca05d74865d
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:35:11 +0300

    js_261

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-let elems = document.querySelectorAll("p");
-for (let elem of elems) {
+let list = document.querySelectorAll(".list");
+for (let elem of list) {
     elem.addEventListener("click", func);
-}
+};
 function func() {
-    this.textContent += '!'
-    this.removeEventListener("click", func); 
+    if (Number(this.textContent) < 10) {
+        this.textContent = Number(this.textContent) + 1
+    } else {
+        this.removeEventListener('click', func);
+    }
 }
