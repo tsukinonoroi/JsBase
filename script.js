397e3346312fa62f710424c099f8e7b572a69d15
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 01:45:19 +0300

    js_144

--- script.js
+++ script.js
@@ -1,66 +1,83 @@
 let obj = {
-   key1: {
-      key1: 1,
-      key2: 2,
-      key3: 3,
-   },
-   key2: {
-      key1: 4,
-      key2: 5,
-      key3: 6,
-   },
-   key3: {
-      key1: 7,
-      key2: 8,
-      key3: 9,
-   },
- };
- 
- let sum1 = obj.key1.key1 + obj.key1.key2 + obj.key1.key3;
- let sum2 = obj.key2.key1 + obj.key2.key2 + obj.key2.key3;
- let sum3 = obj.key3.key1 + obj.key3.key2 + obj.key3.key3;
- 
- console.log(sum1 + sum2 + sum3)
- 
- let obj1 = {
    1: {
-      1: 'a1',
-      2: 'a2',
-      3: 'a3',
+      1: 11,
+      2: 12,
+      3: 13,
    },
    2: {
-      1: 'b1',
-      2: 'b2',
-      3: 'b3',
+      1: 21,
+      2: 22,
+      3: 23,
    },
    3: {
-      1: 'c1',
-      2: 'c2',
-      3: 'c3',
+      1: 24,
+      2: 25,
+      3: 26,
    },
  };
  
- console.log(obj1[2][2]);
- console.log(obj1[3][1]);
+ let result = 0;
+ 
+ for (let key in obj) {
+   let obj3 = obj[key];
+   for (let obj1 in obj3) {
+      result += obj3[obj1]
+   };
+ };
+ 
+ console.log(result)
  
  let obj2 = {
-   key1: {
-      a: 1,
-      b: 2,
-      c: {
-         d: 3,
-         e: 4,
-      }, 
-      f: 5,
+   1: {
+      1: {
+         1: 111,
+         2: 112,
+         3: 113,
+      },
+      2: {
+         1: 121,
+         2: 122,
+         3: 123,
+      },
    },
-   key2: {
-      g: 6,
-      h: 7,
+   2: {
+      1: {
+         1: 211,
+         2: 212,
+         3: 213,
+      },
+      2: {
+         1: 221,
+         2: 222,
+         3: 223,
+      },
+   },
+   3: {
+      1: {
+         1: 311,
+         2: 312,
+         3: 313,
+      },
+      2: {
+         1: 321,
+         2: 322,
+         3: 323,
+      },
    },
  };
  
- let sum11 = obj2.key1.a + obj2.key1.b + obj2.key1.c.d + obj2.key1.c.e + + obj2.key1.f;
- let sum22 = obj2.key2.g + obj2.key2.h;
+ let result1 = 0;
  
+ for (let key in obj2) {
+   let obj_3 = obj2[key];
  
- console.log(sum11 + sum22);
+   for (let key2 in obj_3) {
+      let obj_2 = obj_3[key2];
+ 
+      for (let key3 in obj_2) {
+         result1 += obj_2[key3]
+      }
+   }
+ };
+ 
+ console.log(result1)
