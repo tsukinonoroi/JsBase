51d526b7eb64820bbe7f8a41955ed6e98929c588
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 01:51:08 +0300

    js_152

--- script.js
+++ script.js
@@ -1,75 +1,97 @@
-// №1
+let str = 'js';
+console.log(str.toUpperCase());
 
-console.log(Math.pow(2, 10));
+let str2 = 'JS';
+console.log(str2.toLowerCase());
 
-// №2
+let str3 = 'я учу javascript!';
 
-console.log(Math.sqrt(245));
+let a1 = str3.substr(2, 3);
+let a2 = str3.substr(6, 10);
+console.log(a1, a2);
 
-//№3
 
-let arr = [4, 2, 5, 19, 13, 0, 10];
-let result = 0;
+let b1 = str3.substring(2, 5);
+let b2 = str3.substring(6, 16);
+console.log(b1, b2);
 
-for (let item of arr) {
-   result += (Math.pow(item, 3));
-}
-console.log(Math.sqrt(result));
 
-// №4
+let c1 = str3.slice(2, 5);
+let c2 = str3.slice(6, 16);
+console.log(c1, c2);
 
-let num = 379;
 
-let result1 = Math.round(Math.sqrt(num));
-console.log(result1);
 
-let result2 = Math.sqrt(num);
-console.log(result2.toPrecision(3));
+let str4 = 'abcde';
+console.log(str4.indexOf('c'));
 
-let result3 = Math.sqrt(num);
-console.log(result3.toPrecision(4));
 
-// №5
-
-let num1 = 587;
-
-let obj = {
-   floor: null,
-   ceil: null,
-}
-
-let result4 = Math.sqrt(num1)
-obj.floor = Math.floor(result4);
-
-let result5 = Math.sqrt(num1);
-obj.ceil = Math.ceil(result5);
-
-console.log(obj)
-
-// №6
-
-console.log(Math.max(4, -2, 5, 19, -130, 0, 10));
-console.log(Math.min(4, -2, 5, 19, -130, 0, 10));
-
-// №7
-
-function getRandomInt(min, max) {
-   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-console.log(getRandomInt(1, 100))
-
-// №8
-
-let newArr = [];
-
-for (let i = 1; i <= 10; i++) {
-   newArr.push(getRandomInt(1, 1000));
+let str5 = 'abcde';
+if (str5.indexOf('a') >= 0) {
+   console.log('true')
+} else {
+   console.log('false')
 };
 
-console.log(newArr)
 
-// №9
-let a = -12, b = -6;
+let str6 = 'bcade';
+if (str6.indexOf('a') == 0) {
+   console.log('true')
+} else {
+   console.log('false')
+};
 
-console.log(Math.abs(a - b));
+
+
+let str7 = 'bcdea';
+if (str7.indexOf('a') == str7.length - 1) {
+   console.log('true')
+} else {
+   console.log('false')
+};
+
+
+let str8 = 'http://bcdea';
+if (str8.indexOf('http://') == 0) {
+   console.log('true')
+} else {
+   console.log('false')
+};
+
+
+let str9 = 'http://bcdea.html';
+if (str9.lastIndexOf('.html') == str9.length - 5) {
+   console.log('true')
+} else {
+   console.log('false')
+};
+
+
+let str10 = 'http://abcd.html';
+if (str10.startsWith('http://')) {
+   console.log('true')
+} else {
+   console.log('false')
+};
+
+
+let str11 = 'http://abcd.html';
+if (str11.endsWith('.html')) {
+   console.log('true')
+} else {
+   console.log('false')
+};
+
+
+let str12 = '1-2-3-4-5';
+let newArr = str12.split('-');
+console.log(newArr);
+
+
+let str13 = '12345';
+let newArr1 = str13.split('');
+console.log(newArr1);
+
+
+let arr = [1, 2, 3, 4, 5];
+console.log(arr.join('-'))
