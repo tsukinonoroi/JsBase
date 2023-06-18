0d8ad67cccb7f61342701549df4c8c9e59133aa7
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:33:36 +0300

    js_257

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-let elems = document.querySelectorAll(".elem1");
-let btn = document.querySelector('#button1')
-
-btn.addEventListener('click', () => {
-    console.log(elems)
-    for (let elem of elems) {
-        elem.textContent = 'text';
-    }
-})
+let inputs = document.querySelectorAll(".input");
+for (let input of inputs) {
+    input.addEventListener("blur", func);
+}
+function func() {
+    this.value = Number(this.value) + 1;
+}
+//  2
+let text = document.querySelectorAll(".par");
+for (let elem of text) {
+    elem.addEventListener("click", func1);
+}
+function func1() {
+    this.textContent = Math.pow(Number(this.textContent), 2)
+}
