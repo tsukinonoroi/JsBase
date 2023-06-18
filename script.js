41b1eeb4d9c00ae0aed5d9b5d1eead8ebfd192fb
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:43:38 +0300

    js_287

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
 window.onload = function() {
-   const select = document.querySelector('select');
-   for (let i = 0; i < select.options.length; i++) {
-     const option = select.options[i];
-     if (option.selected) {
-       option.textContent += '!';
-     } else {
-       option.textContent += '?';
-     }
-   }
+   let textarea = document.querySelector('textarea');
+   let div = document.querySelector('div');
+   
+   textarea.addEventListener('input', function() {
+       div.textContent = textarea.value;
+   });
    // 2
-   const mySelect = document.getElementById('mySelect');
-   const myButton = document.getElementById('myButton');
    
-   myButton.addEventListener('click', () => {
-     mySelect.selectedIndex = mySelect.options.length - 1;
-   });
+     let checkbox = document.querySelector('#checkbox');
+     let button = document.querySelector('#button');
+     let div1 = document.querySelector('#div');
+   
+     button.addEventListener('click', function() {
+       if (checkbox.checked) {
+         div1.textContent = '111';
+       } else {
+         div1.textContent = '222';
+       }
+     });
    // 3
-   const select1 = document.getElementById('mySelect');
-   const button = document.getElementById('btn');
-   
-   button.addEventListener('click', function() {
-     const selectedOption = select1.options[select1.selectedIndex];
-     alert(selectedOption.textContent);
-   });
-   // 4
-   let select2 = document.getElementById('mySelect');
-   let button1 = document.getElementById('myButton');
-   
-   button1.addEventListener('click', function() {
-     let selectedOption = select2.options[select2.selectedIndex];
-     selectedOption.textContent += '!';
-   });
+   let checkbox1 = document.querySelector('[type="checkbox"]');
+     let button1 = document.querySelector('button');
+     
+     button1.addEventListener('click', function() {
+       if (checkbox1.checked) {
+         console.log('+++');
+       } else {
+         console.log('---');
+       }
+     });
    }
