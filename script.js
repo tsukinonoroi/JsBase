09eb5dac61af5c9660743a490d9affe786859824
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:54:22 +0300

    js_320

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 let parent = document.querySelector('#parent');
 
-let p = document.createElement('p');
-p.textContent = '!';
-
-p.addEventListener('click', function() {
-	console.log(this.textContent); 
-});
-
-parent.appendChild(p);
+for (let i = 1; i <= 9; i++) {
+	let p = document.createElement('p');
+	p.textContent = '!';
+	
+	parent.appendChild(p);
+}
