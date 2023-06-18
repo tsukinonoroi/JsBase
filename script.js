ded6b218d92af1b8d6c81d393234b1d5da854f2c
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 01:57:47 +0300

    js_167

--- script.js
+++ script.js
@@ -1,10 +1,65 @@
-function func(num1, num2) {
+let arr1 = [2, 4, 6];
 
-   if (num1 > 0 && num2 > 0) {
-      return num1 * num2;
-   } else {
-      return num1 - num2;
-   }
+function func1(arr) {
+
+   for (let elem of arr) {
+      if (elem % 2 != 0) {
+         return false;
+      }
+   };
+
+   return true;
+};
+
+if (func1(arr1)) {
+   console.log('Все числа четные');
+} else {
+   console.log('Есть нечетные числа')
 }
 
-console.log(func(3, 4));
+
+
+
+
+let num = 4663;
+
+function func2(number) {
+
+   let arrNum = String(number).split('')
+
+   for (let elem of arrNum) {
+      if (elem % 2 != 0) {
+         return false;
+      }
+   };
+
+   return true;
+};
+
+if (func2(num)) {
+   console.log('Все цифры четные');
+} else {
+   console.log('Есть нечетные цифры')
+}
+
+
+
+
+let arr3 = [2, 3, 4, 4, 7];
+
+function func3(arr) {
+   for (let i = 0; i <= arr.length - 1; i++) {
+      if (arr[i] == arr[i+1]) {
+         return true;
+      } else {
+         continue;
+      }
+   };
+   return false;
+}
+
+if (func3(arr3)) {
+   console.log('Есть два одинаковых элемента подряд');
+} else {
+   console.log('Нет двух одинаковых элемента подряд')
+}
