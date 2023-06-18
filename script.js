800a3cdac91177073ef893681709751fa57e8cda
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:51:11 +0300

    js_310

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-let i = 100;
-setInterval(() => console.log(--i), 1000);
+let i = 10;
+
+let timerId = setInterval(function() {
+	console.log(--i);
+	
+	if (i >= 10) {
+		clearInterval(timerId);
+	}
+}, 1000);
