bf41698e821e9b317437811f08d8a86bcf08e0e0
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:07:01 +0300

    js_193

--- script.js
+++ script.js
@@ -1,165 +1,51 @@
-!(function () {
-   console.log("!");
-})(); // !
+let func = (function () {
+   let num = 1;
 
-
-
- 
-
-
-
-let result = (function () {
-   return "!";
-})();
-
-console.log(result); // !
-
-
-
-
-
-
-let result4 = function () {
-   return "!";
-};
-
-console.log(result4); // код функции
-
-
-
-
-
-
-let result5 = function () {
-   return "!";
-};
-
-console.log(result5()); // !
-
-
-
-
-
-
-let result6 =
-   (function () {
-      return 1;
-   })() +
-   (function () {
-      return 2;
-   })();
-
-console.log(result6); // 3
-
-
-
-
-
-
-let result7 = (function () {
-   return "!";
-})();
-
-console.log(result7); // !
-
-
-
-
-
-
-let result8 = (function () {
-   return "!";
-})();
-
-console.log(result8); // !
-
-
-
-
-
-
-
-let result9 = function () {
-   return "!";
-};
-
-console.log(result9); // код функции
-
-
-
-
-
-
-let result10 = function () {
-   return "!";
-};
-
-console.log(result10()); // !
-
-
-
-
-
-
-(function (num1, num2) {
-   console.log(num1 + num2);
-})(1, 2); // 3
-
-
-
-
-
-
-
-(function () {
    return function () {
-      return function () {
-         console.log("!");
-      };
+      console.log(num);
+      num++;
    };
-})()()();
+})();
+
+func();
+func();
+func();
+func();
+func();
 
 
 
 
 
 
-(function (num1) {
-   return function (num2) {
-      console.log(num1 + num2);
+
+let func5 = (function () {
+   let num = 1;
+   return function () {
+      if (num == 5) {
+         console.log(num);
+         num = 1;
+      } else {
+         console.log(num);
+         num++;
+      }
    };
-})(1)(2);
-
-
-
-
-
-
-(function (num1) {
-   return function (num2) {
-      return function (num3) {
-         console.log(num1 + num2 + num3);
-      };
-   };
-})(1)(2)(3);
-
-
-
-
-
-let str = "str";
-
-(function () {
-   console.log(1);
-})(); // 1
-
-
-
-
-
-
-let str3 = "str"
-
-(function () {
-   console.log(1);
-})(); // error
+})();
+func5();
+func5();
+func5();
+func5();
+func5();
+func5();
+func5();
+func5();
+func5();
+func5();
+func5();
+func5();
+func5();
+func5();
+func5();
+func5();
+func5();
+func5();
