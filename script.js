243f0b312833b183b24447ba665fdd25337dcf35
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:12:16 +0300

    js_209

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let arr = ["Johsn", "Smit", "development", "programmer", 2000];
-let [name, surname, department, position, salary] = arr;
+function func() {
+	return ['John', 'Smit', 'development', 
+		'programmer', 2000]; 
+}
+let [name, surname, department, position, salary] = func();
 console.log(surname);
 console.log(name);
