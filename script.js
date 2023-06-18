63500d0f8973b1ceec422bc5d19c0f567a1a48ba
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:36:59 +0300

    js_267

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-let parent = document.querySelector('#elem');
-let text = parent.firstElementChild.innerHTML;
-console.log(text);
+let elem = document.getElementById('elem');
+elem.textContent = 'text'
+//   2
+let elems = document.getElementsByTagName('li');
+for (let elem of elems) {
+    elem.textContent += '!'
+}
+//   3
+let elems2 = document.getElementsByClassName('www');
+for (let elem of elems2) {
+    elem.innerHTML = '!!!';
+}
