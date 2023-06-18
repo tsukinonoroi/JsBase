2f823399b7f549a5d4a2043388ba7da11d593bea
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:06:27 +0300

    js_191

--- script.js
+++ script.js
@@ -1,57 +1,172 @@
-function test() {
-   let num1 = 1;
-   let num2 = 2;
+function test1() {
+   let num = 1;
 
    return function () {
-      return num1 + num2;
+      console.log(num);
+      num++;
    };
 }
 
-let func = test();
-console.log(func()); // 3
+let func = test1();
+func();
+func();
+
+
+
+
+
+function test3() {
+   let num = 10;
+
+   return function () {
+      console.log(num);
+      num--;
+   };
+}
+
+let func3 = test3();
+
+func3();
+func3();
 
 
 
 
 
 function test2() {
-   let num1 = 1;
-   let num2 = 2;
+   let num = 10;
 
    return function () {
-      return num1 + num2;
+      if (num >= 0) {
+         console.log(num);
+         num--;
+      } else {
+         console.log("Конец");
+      }
    };
 }
 
-console.log(test2()()); // 3
+let func2 = test2();
+
+func2();
+func2();
+func2();
+func2();
+func2();
+func2();
+func2();
+func2();
+func2();
+func2();
+func2();
+func2();
 
 
 
 
-function test3() {
-   let num1 = 1;
+
+function func4() {
+   let num = 0;
 
    return function () {
-      return num1 + num2;
+      console.log(num);
+      num++;
    };
 }
 
-let num2 = 2;
-let func3 = test3();
-console.log(func3()); // 3
+func4()(); // 0
+func4()(); // 0
+func4()(); // 0
 
 
 
 
 
-function test4() {
-   let num = 1;
+function func5() {
+   let num = 0;
 
    return function () {
-      return num;
+      console.log(num);
+      num++;
    };
 }
 
-let num = 2;
-let func4 = test4();
-console.log(func4()); // 1
+let test5 = func5;
+
+test5()(); // 0
+test5()(); // 0
+test5()(); // 0
+
+
+
+
+
+let counter = 0;
+
+function test55() {
+   return function () {
+      console.log(counter);
+      counter++;
+   };
+}
+
+let func55 = test55;
+
+let func56 = func55();
+let func57 = func55();
+
+func56(); // 0
+func57(); // 1
+func56(); // 2
+func57(); // 3
+
+
+
+
+
+function test66() {
+   let counter = 0;
+
+   return function () {
+      return function () {
+         console.log(counter);
+         counter++;
+      };
+   };
+}
+
+let func66 = test66()();
+
+let func67 = func66;
+let func68 = func66;
+
+func67(); // 0
+func68(); // 1
+func67(); // 2
+func68(); 
+
+
+
+
+
+
+function test80() {
+   let counter = 0;
+
+   return function () {
+      return function () {
+         console.log(counter);
+         counter++;
+      };
+   };
+}
+
+let func80 = test80();
+
+let func81 = func80();
+let func82 = func80();
+
+func81(); // 0
+func82(); // 1
+func81(); // 2
+func82(); // 3
