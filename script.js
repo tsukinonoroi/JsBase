111283331c062e028d262a5cf89ee2ca3a7203e1
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:39:19 +0300

    js_274

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-var elem = document.getElementById("task");
-elem.addEventListener("click", func);
-var elem1 = document.getElementsByClassName("cls");
-function func() {
-    var sum = 0;
-    for (var i = 0; i < elem1.length; i++) {
-        sum += +elem1[i].value;
-    }
-    var newElem = document.getElementById("sum");
-            newElem.value = sum;
-}
+let elem = document.querySelector('#elem');
+let p = document.querySelector('#p')
+
+elem.addEventListener('blur', () => {
+    p.textContent = elem.textContent
+})
