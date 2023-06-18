8e3e3270aa2bdc8612bbe2b4c17437af2082f36b
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:18:41 +0300

    js_212

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-let arr = ["Johsn", "Smit", "development", "programmer", 2000];
-let [name, surname, ...info] = arr;
+let arr = ["John", "Smit", "development"];
+
+let [name, surname, department, programmer = 'programmer', year = 200] = arr
+
 console.log(name)
 console.log(surname)
-console.log(info)
+console.log(department)
+console.log(programmer)
+console.log(year)
