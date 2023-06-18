5d34613c0137866eb1878f92052db65514cd38b0
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:41:33 +0300

    js_281

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-let elem = document.querySelector("#elem");
-let p = document.querySelector('p')
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
 
-elem.addEventListener("input", function () {
-    p.textContent = this.value;
+input1.addEventListener('keyup', function() {
+  if (this.value.length >= 2) {
+    input2.focus();
+  }
+});
+
+input2.addEventListener('keyup', function() {
+  if (this.value.length >= 2) {
+    this.blur();
+  }
 });
