ed73ca3cdac6490ea48ae6edd088eb20ab496a5a
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 01:50:46 +0300

    js_151

--- script.js
+++ script.js
@@ -1,39 +1,75 @@
-let affairs = {
-   '2019-12-28': ['data11', 'data12', 'data13'],
-   '2019-12-29': ['data21', 'data22', 'data23'],
-   '2019-12-30': ['data31', 'data32', 'data33'],
- };
- 
- affairs['2019-12-29'].push('data24');
- 
- console.log(affairs)
- 
- affairs['2019-12-31'] = ['data41', 'data42'];
- 
- console.log(affairs)
- 
- let students = {
-   'group1': {
-      'subgroup11': ['student111', 'student112', 'student113'],
-      'subgroup12': ['student121', 'student122', 'student123'],
-   },
-   'group2': {
-      'subgroup21': ['student211', 'student212', 'student213'],
-      'subgroup22': ['student221', 'student222', 'student223'],
-   },
-   'group3': {
-      'subgroup31': ['student311', 'student312', 'student313'],
-      'subgroup32': ['student321', 'student322', 'student323'],
-   },
- };
- 
- students['group1']['subgroup11'].push('student113')
- 
- console.log(students)
- 
- students['group1']['subgroup13'] = ['student131', 'student132', 'student133'];
- 
- students['group4'] = {};
- students['group4']['subgroup41'] = ['student431', 'student432', 'student433'];
- 
- console.log(students)
+// №1
+
+console.log(Math.pow(2, 10));
+
+// №2
+
+console.log(Math.sqrt(245));
+
+//№3
+
+let arr = [4, 2, 5, 19, 13, 0, 10];
+let result = 0;
+
+for (let item of arr) {
+   result += (Math.pow(item, 3));
+}
+console.log(Math.sqrt(result));
+
+// №4
+
+let num = 379;
+
+let result1 = Math.round(Math.sqrt(num));
+console.log(result1);
+
+let result2 = Math.sqrt(num);
+console.log(result2.toPrecision(3));
+
+let result3 = Math.sqrt(num);
+console.log(result3.toPrecision(4));
+
+// №5
+
+let num1 = 587;
+
+let obj = {
+   floor: null,
+   ceil: null,
+}
+
+let result4 = Math.sqrt(num1)
+obj.floor = Math.floor(result4);
+
+let result5 = Math.sqrt(num1);
+obj.ceil = Math.ceil(result5);
+
+console.log(obj)
+
+// №6
+
+console.log(Math.max(4, -2, 5, 19, -130, 0, 10));
+console.log(Math.min(4, -2, 5, 19, -130, 0, 10));
+
+// №7
+
+function getRandomInt(min, max) {
+   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(getRandomInt(1, 100))
+
+// №8
+
+let newArr = [];
+
+for (let i = 1; i <= 10; i++) {
+   newArr.push(getRandomInt(1, 1000));
+};
+
+console.log(newArr)
+
+// №9
+let a = -12, b = -6;
+
+console.log(Math.abs(a - b));
