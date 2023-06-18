7b51d9c6140e1f0f0741bf7c3f6b8ce09f583a0f
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:54:40 +0300

    js_321

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ for (let i = 1; i <= 9; i++) {
 	let p = document.createElement('p');
 	p.textContent = '!';
 	
+	// Навешиваем обработчик клика:
+	p.addEventListener('click', function() {
+		console.log(this.textContent);
+	});
+	
 	parent.appendChild(p);
 }
