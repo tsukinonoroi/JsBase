d4389821a62c1b05a7d37869dd2d5c133e05e6e2
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:47:21 +0300

    js_298

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-let parent = document.querySelector('#parent');
-let button = document.querySelector('button');
-let block  = document.querySelector('#block');
+elem1.addEventListener('click', function() {
+	console.log('зеленый - погружение');
+}, true);
+elem1.addEventListener('click', function() {
+	console.log('зеленый - всплытие');
+}, false);
 
-button.addEventListener('click', function() {
-	block.classList.add('active');
-});
-parent.addEventListener('click', function() {
-	block.classList.remove('active');
-});
+elem2.addEventListener('click', function() {
+	console.log('голубой - погружение');
+}, true);
+elem2.addEventListener('click', function() {
+	console.log('голубой - всплытие');
+}, false);
+
+elem3.addEventListener('click', function() {
+	console.log('красный - погружение');
+}, true);
+elem3.addEventListener('click', function() {
+	console.log('красный- всплытие');
+}, false);
