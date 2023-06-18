6f2f58530028706fa29cc77a97bab19617a3ff50
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:39:53 +0300

    js_276

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
 let elem = document.querySelector('#elem');
-let button = document.querySelector('.btn')
-
-button.addEventListener('click', () => {
-    elem.disabled = !elem.disabled
-})
+console.log(elem.checked);
