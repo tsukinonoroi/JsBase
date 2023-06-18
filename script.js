158b257d87856e30f8d7af50876421dd32a903b7
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:27:16 +0300

    js_236

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-let now = new Date();
-let year = now.getFullYear();
-for (let month = 0; month <= 11; month++) {
-let date = new Date(year, month, 1);
-if (date.getDay() == 0) {
-    console.log(year + "-" + month + "-1");
+let day = new Date(2023, 4, 2, 23);
+let now = new Date(2023, 4, 3, 15);
+let diff1 = now - day;
+console.log(diff1 / 1000);
+// 2
+let day1 = new Date(2023, 4, 3, 23);
+let now1 = new Date(2023, 4, 3, 15);
+let diff2 = day1 - now1;
+console.log(diff2 / 1000);
+// 3
+let day2 = new Date(2023, 12, 31, 23);
+let now2 = new Date(2023, 4, 3, 15);
+let diff3 = day2 - now2;
+console.log(diff3 / 1000 / 60 / 60 / 24);
+// 4
+function isLeap() {
+    if (new Date().getFullYear() == 12) {
+        return true;
+    } else {
+        return false;
+    }
 }
+    if (isLeap == true) {
+        console.log("Високосный");
+    } else {
+        console.log("Обычный");
 }
