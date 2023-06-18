f2a78e168a85f130e1d54a9719fdcb3c9ad3ba2b
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 02:58:29 +0300

    js_334

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-let table = document.querySelector('#table');
+let tds = document.querySelectorAll('#table td');
 
-let tr = document.createElement('tr');
-
-for (let i = 1; i <= 3; i++) {
-	let td = document.createElement('td');
-	tr.appendChild(td);
-}
-
-table.appendChild(tr);
-
-let trs = document.querySelectorAll('#table tr');
-
-for (let tr of trs) {
-	let td = document.createElement('td');
-	tr.appendChild(td);
+for (let td of tds) {
+	td.textContent = '!';
 }
