5246a668429ac74e26cfd4786bd2f3332c56a08c
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 03:03:28 +0300

    js_350

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-function setText(selector, text) {
+forEach('.elem', function(elem) {
+	elem.textContent = elem.textContent ** 2;
+});
+
+function forEach(selector, func) {
 	let elems = document.querySelectorAll(selector);
 	
 	for (let elem of elems) {
-		elem.textContent = text;
+		func(elem);
 	}
 }
-
-setText('.elem', 'text');
