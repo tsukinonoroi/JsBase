573c56d75286e980a9d0f5bc0f3da8a8f5f79971
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 03:04:00 +0300

    js_352

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-forEach('.elem', function(elem, index) {
-	console.log(elem);  // выведет элемент
-	console.log(index); // выведет порядковый номер элемента
-});
-forEach('.elem', function(elem, index) {
-	elem.textContent = elem.textContent + index;
-});
-function forEach(selector, func) {
-	let elems = document.querySelectorAll(selector);
-	
-	for (let i = 0; i < elems.length; i++) {
-		func(elems[i], i);
-	}
+function setText(elem, text){
+	elem.textContent = text;
 }
+let elem1 = document.getElementById('elem1');
+setText(elem1, 'text1');
+
+let elem2 = document.getElementById('elem2');
+setText(elem2, 'text2');
