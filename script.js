452ff9f6ebf536677b5f0eb54973e594151ce07f
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 03:03:42 +0300

    js_351

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-forEach('.elem', function(elem) {
-	elem.textContent = elem.textContent ** 2;
+forEach('.elem', function(elem, index) {
+	console.log(elem);  // выведет элемент
+	console.log(index); // выведет порядковый номер элемента
 });
-
+forEach('.elem', function(elem, index) {
+	elem.textContent = elem.textContent + index;
+});
 function forEach(selector, func) {
 	let elems = document.querySelectorAll(selector);
 	
-	for (let elem of elems) {
-		func(elem);
+	for (let i = 0; i < elems.length; i++) {
+		func(elems[i], i);
 	}
 }
