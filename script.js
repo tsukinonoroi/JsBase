970013daab3595f72689c63844dffe39bd138293
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 03:04:13 +0300

    js_353

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-function setText(elem, text){
-	elem.textContent = text;
+function setText(elems, text) {
+	for (let elem of elems) {
+		elem.textContent = text;
+	}
 }
-let elem1 = document.getElementById('elem1');
-setText(elem1, 'text1');
-
-let elem2 = document.getElementById('elem2');
-setText(elem2, 'text2');
+let elems = document.querySelectorAll('.elem');
+setText(elems, 'text65');
