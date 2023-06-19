a996f99643220e69de88aec6636b1d930bd17da2
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 03:04:41 +0300

    js_355

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let div1 = document.querySelector('#elem1');
-createTable(3, 4, div1);
+let div = document.querySelector('#elem');
 
-let div2 = document.querySelector('#elem2');
-createTable(5, 6, div2);
+let table = createTable(3, 4);
+table.style.color = 'red';
+
+div.appendChild(table);
