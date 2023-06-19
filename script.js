17e5256a15035772b5a06c607eafb4c80700c54d
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 03:04:55 +0300

    js_356

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let div = document.querySelector('#elem');
 
-let table = createTable(3, 4);
-table.style.color = 'red';
+let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+let table = createTableByArr(arr);
 
 div.appendChild(table);
