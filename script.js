c68da1a0038e820f75c3d0d44f1e4a54ba4cfac9
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 03:06:17 +0300

    js_361

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-;(function(root) {
+;(function(root, type, amount) {
 	let parent = document.querySelector(root);
 	
-	let div1 = parent.querySelector('#div1');
-	let div2 = parent.querySelector('#div2');
-	let div3 = parent.querySelector('#div3');
-	
-	let res  = parent.querySelector('#res');
-	let btn  = parent.querySelector('#btn');
-	
-	btn.addEventListener('click', function() {
-		let num1 = Number(div1.textContent);
-		let num2 = Number(div2.textContent);
-		let num3 = Number(div3.textContent);
-		
-		res.textContent = num1 + num2 + num3;
-	});
-})('#parent');
+	for (let i = 1; i <= amount; i++) {
+		let elem = document.createElement(type);
+		parent.append(elem);
+	}
+})('#parent', 'p', 5);
+
+let config = {
+	root:  '#parent',
+	type:  'p',
+	amount: 5
+}
