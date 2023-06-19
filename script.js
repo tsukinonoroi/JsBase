89ff422025c8fc2c92534bd4802ed031aeb77365
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 03:06:53 +0300

    js_363

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 ;(function() {
-	let str = 'переменная модуля';
-	
-	function func() {
-		alert('функция модуля');
+	function square(num) {
+		return num ** 2;
+	}
+	function cube(num) {
+		return num ** 3;
+	}
+	function avg(arr) {
+		return sum(arr, 1) / arr.length;
+	}
+	function digitsSum(num) {
+		return sum(String(num).split(''));
 	}
 	
-	window.func = func;
+	// вспомогательная функция
+	function sum(arr) {
+		let res = 0;
+		
+		for (let elem of arr) {
+			res += +elem;
+		}
+		
+		return res;
+	}
+	
+	window.math = {square, cube, avg, digitsSum};
 })();
-
-func(); 
