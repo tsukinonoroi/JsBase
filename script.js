d55703b610eca68dd9f9997311ec306390464aef
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 03:02:13 +0300

    js_346

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let tds = document.querySelectorAll('#table td');
 
+let color = 'color1';
 for (let td of tds) {
 	td.addEventListener('click', function() {
-		this.classList.add('active');
+		if (color == 'color1') {
+			color = 'color2'
+		} else {
+			color = 'color1'
+		}
+		
+		this.classList.add(color);
 	});
 }
